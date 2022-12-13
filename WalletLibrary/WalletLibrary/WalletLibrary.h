b97f8cbf67fbc49ba54664c986add8b963134127
Sydney <symorton@microsoft.com>
2022-12-13 14:18:46 -0500

    Create Initial Folder Structure for Library (#1)
    * Add submodule
    
    * Add git submodule for VC SDK
    
    * Create workspace
    
    * Create test app
    
    * Fix nit for initial PR
    
    * Add code owners and pull request template

---
+++ WalletLibrary/WalletLibrary/WalletLibrary.h
@@ -0,0 +1,16 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WalletLibrary.
+FOUNDATION_EXPORT double WalletLibraryVersionNumber;
+
+//! Project version string for WalletLibrary.
+FOUNDATION_EXPORT const unsigned char WalletLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WalletLibrary/PublicHeader.h>
+
+
---
+++ WalletLibrary/WalletLibraryTests/VerifiedIdFlowTests.swift
@@ -0,0 +1,30 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+import XCTest
+@testable import WalletLibrary
+
+class VerifiedIdFlowTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testInitiateFlow() throws {
+        print(VerifiedIdFlow().initiate())
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
