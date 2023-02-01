af7d9dac4b9065d3b253c4fb41906965d0419c99
Sydney Morton <symorton@microsoft.com>
2023-02-01 10:54:02 -0800

    Add ResolvedInput

---
+++ WalletLibrary/WalletLibrary/Protocols/Requests/ResolvedInput/ResolvedInput.swift
@@ -0,0 +1,13 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+public protocol ResolvedInput {
+    
+    var style: RequesterStyle { get }
+    
+    var requirement: Requirement { get }
+
+    var rootOfTrust: RootOfTrust { get }
+}
