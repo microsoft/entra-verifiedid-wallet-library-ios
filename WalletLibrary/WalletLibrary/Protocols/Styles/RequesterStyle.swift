6774170ff68ad0cd1f27c3ff3e5a32d447892c22
Sydney Morton <symorton@microsoft.com>
2023-02-01 13:35:59 -0800

    Add Style protocol

---
+++ WalletLibrary/WalletLibrary/Protocols/Styles/RequesterStyle.swift
@@ -0,0 +1,13 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+/**
+ * The look and feel of the requester who has initiated the request.
+ */
+public protocol RequesterStyle {
+    /// The name of the requester.
+    var requester: String { get }
+}
+
