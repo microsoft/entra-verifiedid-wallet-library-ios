b8f5f5f39d4ed55665896fbe664a3f7cd3834428
Sydney Morton <symorton@microsoft.com>
2023-02-01 13:35:51 -0800

    Add Client Input Protocol

---
+++ WalletLibrary/WalletLibrary/Protocols/Requests/VerifiedIdClientInput.swift
@@ -0,0 +1,9 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+/**
+ * Input to initiate a Verified Id Flow.
+ */
+public protocol VerifiedIdClientInput { }
