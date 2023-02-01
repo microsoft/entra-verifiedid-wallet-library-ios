c93aafb82401b61103ee6900c0cd116563792cee
Sydney Morton <symorton@microsoft.com>
2023-02-01 13:35:24 -0800

    Mock out demo app.

--- Demo/WalletLibraryDemo/WalletLibraryDemo/ContentView.swift
+++ Demo/WalletLibraryDemo/WalletLibraryDemo/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            Text(VerifiedIdFlow().initiate())
+            Text("Hello World")
         }
     }
 }
