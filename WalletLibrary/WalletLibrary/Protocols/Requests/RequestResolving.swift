277e612a6f69b2907f5c1e764b87a700d934dd0a
Sydney Morton <symorton@microsoft.com>
2023-02-06 13:53:40 -0800

    Add Request Resolving protocol

---
+++ WalletLibrary/WalletLibrary/Protocols/Requests/RequestResolving.swift
@@ -0,0 +1,20 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+/**
+ * An object that conforms to this protocol can resolve a raw request from a request input.
+ * It should be specific to a certain type of request handler and input type.
+ */
+protocol RequestResolving {
+
+    /// Whether or not the object can resolve a raw request that will be handled by the given handler.
+    func canResolve(using handler: RequestHandling) -> Bool
+    
+    /// Whether or not the object can resolve the given input.
+    func canResolve(input: VerifiedIdClientInput) -> Bool
+    
+    /// Resolve the raw request from the given input.
+    func resolve(input: VerifiedIdClientInput) async throws -> RawRequest
+}
