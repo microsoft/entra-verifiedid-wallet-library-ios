2df0ff7a1b0a9d9a3b24e160a741955f68c4f9a1
Sydney Morton <symorton@microsoft.com>
2023-02-09 10:52:38 -0800

    Create OpenIdRequesterStyle.swift

---
+++ WalletLibrary/WalletLibrary/Requests/RequestProtocols/OpenId/OpenIdRequesterStyle.swift
@@ -0,0 +1,11 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+/**
+ * Requester Style that is Open Id specific.
+ */
+struct OpenIdRequesterStyle: RequesterStyle {
+    let requester: String
+}
