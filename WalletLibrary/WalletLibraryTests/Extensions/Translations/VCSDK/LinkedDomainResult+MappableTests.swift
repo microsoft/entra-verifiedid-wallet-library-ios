51eedab612851979c023f23fd8bdbac6a34954f9
Sydney Morton <symorton@microsoft.com>
2023-02-09 15:04:18 -0800

    Add tests for LinkedDomainResult mapper.

---
+++ WalletLibrary/WalletLibraryTests/Extensions/Translations/VCSDK/LinkedDomainResult+MappableTests.swift
@@ -0,0 +1,51 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import XCTest
+import VCEntities
+@testable import WalletLibrary
+
+class LinkedDomainResultMappingTests: XCTestCase {
+    
+    let mapper = Mapper()
+    
+    func testMap_WithLinkedDomainMissing_ReturnsRootOfTrust() throws {
+        // Arrange
+        let linkedDomainResult = LinkedDomainResult.linkedDomainMissing
+        let expectedResult = RootOfTrust(verified: false, source: nil)
+        
+        // Act
+        let actualResult = try mapper.map(linkedDomainResult)
+        
+        // Assert
+        XCTAssertEqual(actualResult, expectedResult)
+    }
+    
+    func testMap_WithLinkedDomainVerified_ReturnsRootOfTrust() throws {
+        // Arrange
+        let mockDomain = "verifiedDomain342"
+        let linkedDomainResult = LinkedDomainResult.linkedDomainVerified(domainUrl: mockDomain)
+        let expectedResult = RootOfTrust(verified: true, source: mockDomain)
+        
+        // Act
+        let actualResult = try mapper.map(linkedDomainResult)
+        
+        // Assert
+        XCTAssertEqual(actualResult, expectedResult)
+    }
+    
+    func testMap_WithLinkedDomainUnverified_ReturnsRootOfTrust() throws {
+        // Arrange
+        let mockDomain = "unverifiedDomain235"
+        let linkedDomainResult = LinkedDomainResult.linkedDomainUnverified(domainUrl: mockDomain)
+        let expectedResult = RootOfTrust(verified: false, source: mockDomain)
+        
+        // Act
+        let actualResult = try mapper.map(linkedDomainResult)
+        
+        // Assert
+        XCTAssertEqual(actualResult, expectedResult)
+    }
+}
