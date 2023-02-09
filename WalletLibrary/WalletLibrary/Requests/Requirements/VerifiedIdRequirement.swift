de61aace5bacd547e46275cf71b61b88fb2fee8e
Sydney Morton <symorton@microsoft.com>
2023-02-09 13:50:51 -0800

    Update VerifiedIdRequirement.swift

--- WalletLibrary/WalletLibrary/Requests/Requirements/VerifiedIdRequirement.swift
+++ WalletLibrary/WalletLibrary/Requests/Requirements/VerifiedIdRequirement.swift
@@ -17,14 +17,11 @@ public class VerifiedIdRequirement: Requirement {
     /// The type of the verified Id required.
     public let types: [String]
     
-    /// The accepted issuers for verified Id required.
-    public let acceptedIssuers: [String]
-    
     /// The purpose for the verified Id, developer can display to the user if desired.
     public let purpose: String?
     
     /// An optional property for information needed for issuance during presentation flow.
-    public let issuanceOptions: IssuanceOptions?
+    public let issuanceOptions: [VerifiedIdRequestInput]
     
     /// TODO: helper method that returns verified id that match the requirement from a list of verified ids.
     public func getMatches(verifiedIds: [VerifiedId]) -> [VerifiedId] {
@@ -34,13 +31,11 @@ public class VerifiedIdRequirement: Requirement {
     init(encrypted: Bool,
          required: Bool,
          types: [String],
-         acceptedIssuers: [String],
          purpose: String?,
-         issuanceOptions: IssuanceOptions?) {
+         issuanceOptions: [VerifiedIdRequestInput]) {
         self.encrypted = encrypted
         self.required = required
         self.types = types
-        self.acceptedIssuers = acceptedIssuers
         self.purpose = purpose
         self.issuanceOptions = issuanceOptions
     }
