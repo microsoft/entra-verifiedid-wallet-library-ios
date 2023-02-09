7d7d46f570e71c26381b848db3470c269a9b4e2b
Sydney Morton <symorton@microsoft.com>
2023-02-09 10:52:44 -0800

    Create GroupRequirement.swift

---
+++ WalletLibrary/WalletLibrary/Requests/Requirements/GroupRequirement.swift
@@ -0,0 +1,33 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+/**
+ * Information to describe a group of requirements.
+ */
+public class GroupRequirement: Requirement {
+    
+    /// If the requirement is required or not.
+    public let required: Bool
+    
+    public let requirements: [Requirement]
+    
+    public let requirementsOperator: Operator
+    
+    init(required: Bool,
+         requirements: [Requirement],
+         requirementsOperator: Operator) {
+        self.required = required
+        self.requirements = requirements
+    }
+    
+    public func validate() throws {
+        throw VerifiedIdClientError.TODO(message: "implement")
+    }
+}
+
+public enum Operator {
+    case ANY
+    case ALL
+}
