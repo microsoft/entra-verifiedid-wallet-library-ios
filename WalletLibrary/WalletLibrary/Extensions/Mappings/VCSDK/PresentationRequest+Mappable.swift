811a8f4aa24c6582754e53ea13dbb7138679f488
Sydney Morton <symorton@microsoft.com>
2023-02-09 13:50:19 -0800

    Update PresentationRequest+Mappable.swift

--- WalletLibrary/WalletLibrary/Extensions/Mappings/VCSDK/PresentationRequest+Mappable.swift
+++ WalletLibrary/WalletLibrary/Extensions/Mappings/VCSDK/PresentationRequest+Mappable.swift
@@ -18,14 +18,19 @@ extension VCEntities.PresentationRequest: Mappable {
     func map(using mapper: Mapping) throws -> VerifiedIdRequestContent {
         
         guard let presentationDefinition = content.claims?.vpToken?.presentationDefinition else {
+            throw PresentationRequestMappingError.presentationDefinitionMissingInRequest
         }
         
         let requirement = try mapper.map(presentationDefinition)
-        let requesterStyle = OpenIdRequesterStyle(requester: "test")
-        let rootOfTrust = try mapper.map(self.linkedDomainResult)
+        let rootOfTrust = try mapper.map(linkedDomainResult)
+        
+        let clientName = content.registration?.clientName ?? ""
+        let requesterStyle = OpenIdRequesterStyle(requester: clientName)
         
         let content = VerifiedIdRequestContent(style: requesterStyle,
                                                requirement: requirement,
                                                rootOfTrust: rootOfTrust)
         
         return content
+    }
+}
