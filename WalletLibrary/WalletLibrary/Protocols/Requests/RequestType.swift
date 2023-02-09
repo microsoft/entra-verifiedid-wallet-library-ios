507a8de4029194b9c7633eb68a11e70282b90052
Sydney Morton <symorton@microsoft.com>
2023-02-09 08:46:59 -0800

    Create RequestType.swift

---
+++ WalletLibrary/WalletLibrary/Protocols/Requests/RequestType.swift
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+/**
+ * The request types that the library supports.
+ */
+enum RequestType {
+    case Presentation
+    case Issuance
+}
