2afc5eb3d0303af5b9e14f5d2b6828e1089b26e5
Sydney Morton <symorton@microsoft.com>
2023-02-09 16:41:44 -0800

    Update MockMapper.swift

--- WalletLibrary/WalletLibraryTests/Mocks/Utilities/MockMapper.swift
+++ WalletLibrary/WalletLibraryTests/Mocks/Utilities/MockMapper.swift
@@ -7,24 +7,20 @@
 
 struct MockMapper: Mapping {
     
-    let error: Error?
+    let callback: ((Any) throws -> (Any?))?
     
-    let returnedObject: Any?
-    
-    init(error: Error? = nil, returnedObject: Any? = nil) {
-        self.error = error
-        self.returnedObject = returnedObject
+    init(callback: ((Any) throws -> (Any?))? = nil) {
+        self.callback = callback
     }
     
     /// Map one object to another.
     func map<T: Mappable>(_ object: T) throws -> T.T {
         
-        if let error = error {
-            throw error
-        }
-        
-        if let returnedObject = returnedObject as? T.T {
-            return returnedObject
+        if let callback = callback {
+            let result = try callback(object)
+            if let result = result as? T.T {
+                return result
+            }
         }
         
         return try object.map(using: self)
