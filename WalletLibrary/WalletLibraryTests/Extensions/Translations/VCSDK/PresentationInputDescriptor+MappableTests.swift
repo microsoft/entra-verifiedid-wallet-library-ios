d6e49ce4367a9d7fdb5f4021694c9a8e87b2376b
Sydney Morton <symorton@microsoft.com>
2023-02-09 16:42:18 -0800

    Create PresentationInputDescriptor+MappableTests.swift

---
+++ WalletLibrary/WalletLibraryTests/Extensions/Translations/VCSDK/PresentationInputDescriptor+MappableTests.swift
@@ -0,0 +1,25 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import XCTest
+import VCEntities
+@testable import WalletLibrary
+
+class PresentationInputDescriptorMappingTests: XCTestCase {
+    
+    let mapper = Mapper()
+    
+    func testMap_WhenWithAllDefaultInput_ReturnsVerifiedIdRequirement() throws {
+        // Arrange
+//        let (input, expectedResult) = try setUpInput(encrypted: false, required: false)
+        
+        // Act
+//        let actualResult = try mapper.map(input)
+        
+        // Assert
+//        assertEqual(actualResult, expectedResult)
+    }
+    
+}
