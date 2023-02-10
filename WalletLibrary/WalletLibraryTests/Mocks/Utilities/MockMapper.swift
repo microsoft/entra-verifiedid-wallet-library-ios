882e00299eb003e23595904d70904f2c9833236f
Sydney Morton <symorton@microsoft.com>
2023-02-10 08:52:34 -0800

    add comment to mock mapper.

--- WalletLibrary/WalletLibraryTests/Mocks/Utilities/MockMapper.swift
+++ WalletLibrary/WalletLibraryTests/Mocks/Utilities/MockMapper.swift
@@ -5,19 +5,28 @@
 
 @testable import WalletLibrary
 
+/**
+ * Mock Mapper can be used to mock results of a mapping using the mockResults callback.
+ * If mockResults callback is not nil, and the result of the call conforms to the T.T protocol,
+ * the result of the callback will be returned. Else, object's map function will be used to map like a normal mapper.
+ *
+ * Using the mockResults callback is helpful when testing the mapping of an object X with recursive mapping.
+ * Since unit tests for object X do not need to test the mapping of objects X contains, the mockResults callback
+ * can be used to return a mock object or mock error instead.
+ */
 struct MockMapper: Mapping {
     
-    let callback: ((Any) throws -> (Any?))?
+    let mockResults: ((Any) throws -> (Any?))?
     
-    init(callback: ((Any) throws -> (Any?))? = nil) {
-        self.callback = callback
+    init(mockResults: ((Any) throws -> (Any?))? = nil) {
+        self.mockResults = mockResults
     }
     
     /// Map one object to another.
     func map<T: Mappable>(_ object: T) throws -> T.T {
         
-        if let callback = callback {
-            let result = try callback(object)
+        if let mockResults = mockResults {
+            let result = try mockResults(object)
             if let result = result as? T.T {
                 return result
             }
