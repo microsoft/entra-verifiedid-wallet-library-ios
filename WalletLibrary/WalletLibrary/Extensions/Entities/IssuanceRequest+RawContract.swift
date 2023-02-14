e70c8733c8a264f24d51535f55f439a5cd331edc
Sydney Morton <symorton@microsoft.com>
2023-02-14 10:47:51 -0800

    Update IssuanceRequest+RawContract.swift

--- WalletLibrary/WalletLibrary/Extensions/Entities/IssuanceRequest+RawContract.swift
+++ WalletLibrary/WalletLibrary/Extensions/Entities/IssuanceRequest+RawContract.swift
@@ -7,9 +7,22 @@ import VCEntities
 
 /**
  * An extension of the VCEntities.IssuanceRequest class.
+ * TODO: Update Style to include VerifiedIdStyle and more requester style attributes.
  */
 extension VCEntities.IssuanceRequest: RawContract {
     func map(using mapper: Mapping) throws -> VerifiedIdRequestContent {
-        throw VerifiedIdClientError.TODO(message: "implement")
+        
+        let attestations = try getRequiredProperty(property: content.input.attestations, propertyName: "Contract Attestations")
+        let requirement = try mapper.map(attestations)
+        let rootOfTrust = try mapper.map(linkedDomainResult)
+        let issuerStyle = IssuerStyle(requester: content.display.card.issuedBy)
+        
+        return VerifiedIdRequestContent(style: issuerStyle,
+                                        requirement: requirement,
+                                        rootOfTrust: rootOfTrust)
     }
+}
+
+struct IssuerStyle: RequesterStyle {
+    var requester: String
 }
