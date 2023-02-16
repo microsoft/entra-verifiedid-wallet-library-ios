99061e52e4a231a5864620865dbd049a04c21c03
Sydney Morton <symorton@microsoft.com>
2023-02-16 14:10:35 -0800

    Update ManifestResolver.swift

--- WalletLibrary/WalletLibrary/Protocols/Requests/Contract/ManifestResolver.swift
+++ WalletLibrary/WalletLibrary/Protocols/Requests/Contract/ManifestResolver.swift
@@ -6,9 +6,10 @@
 import VCEntities
 
 /**
- * Protocol is used as a wrapper to wrap the VC SDK get contract method.
+ * Protocol defines the behavior of taking a url and resolving a manifest.
+ * For example, it is used as a wrapper to wrap the VC SDK get contract method.
  */
 protocol ManifestResolver {
-    /// Fetches and validates the contract.
-    func resolve(with url: String) async throws -> any RawManifest
+    /// Fetches and validates the manifest.
+    func resolve(with url: URL) async throws -> any RawManifest
 }
