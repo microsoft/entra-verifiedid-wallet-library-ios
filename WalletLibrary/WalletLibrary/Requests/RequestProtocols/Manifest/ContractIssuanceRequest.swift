d386804f7900bfbfb2d139e730a12c49d38c8d9f
Sydney Morton <symorton@microsoft.com>
2023-02-15 16:58:24 -0800

    Begin complete implementation on ContractIssuanceRequest

--- WalletLibrary/WalletLibrary/Requests/RequestProtocols/Manifest/ContractIssuanceRequest.swift
+++ WalletLibrary/WalletLibrary/Requests/RequestProtocols/Manifest/ContractIssuanceRequest.swift
@@ -3,6 +3,8 @@
 *  Licensed under the MIT License. See License.txt in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
+import VCEntities
+
 /**
  * Issuance Request that is Contract specific.
  * TODO: we will need contract specific data to implement complete and cancel.
@@ -16,12 +18,25 @@ class ContractIssuanceRequest: VerifiedIdIssuanceRequest {
     
     public let rootOfTrust: RootOfTrust
     
+    private let contractResponder: ContractResponder
+    
     private let configuration: LibraryConfiguration
     
-    init(content: VerifiedIdRequestContent, configuration: LibraryConfiguration) {
+    private let rawContract: any RawContract
+    
+    private let input: VerifiedIdRequestInput
+    
+    init(content: VerifiedIdRequestContent,
+         rawContract: any RawContract,
+         input: VerifiedIdRequestInput,
+         contractResponder: ContractResponder,
+         configuration: LibraryConfiguration) {
         self.style = content.style
         self.requirement = content.requirement
         self.rootOfTrust = content.rootOfTrust
+        self.rawContract = rawContract
+        self.input = input
+        self.contractResponder = contractResponder
         self.configuration = configuration
     }
     
@@ -31,7 +46,19 @@ class ContractIssuanceRequest: VerifiedIdIssuanceRequest {
     }
     
     public func complete() async -> Result<VerifiedId, Error> {
-        return Result.failure(VerifiedIdClientError.TODO(message: "implement"))
+        do {
+            var responseContainer = try IssuanceResponseContainer(from: rawContract, input: input)
+            try responseContainer.add(requirement: requirement)
+            let rawVerifiedId = try await contractResponder.send(requestContent: responseContainer)
+            return Result.success(VerifiedId(id: "test",
+                                             type: "type",
+                                             claims: [],
+                                             expiresOn: Date(),
+                                             issuedOn: Date(),
+                                             raw: rawVerifiedId.raw))
+        } catch {
+            return Result.failure(error)
+        }
     }
     
     public func cancel(message: String?) -> Result<Void, Error> {
