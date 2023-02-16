b65cd298b5a1df1b6933603b6aece2ad76d091a9
Sydney Morton <symorton@microsoft.com>
2023-02-15 16:58:39 -0800

    Update ContractResponder.swift

--- WalletLibrary/WalletLibrary/Protocols/Requests/Contract/ContractResponder.swift
+++ WalletLibrary/WalletLibrary/Protocols/Requests/Contract/ContractResponder.swift
@@ -3,12 +3,10 @@
 *  Licensed under the MIT License. See License.txt in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
-import VCEntities
-
 /**
  * Protocol is used as a wrapper to wrap the VC SDK send response method.
  */
 protocol ContractResponder {
     /// Fetches and validates the contract.
-    func send(response: VCEntities.IssuanceResponseContainer) async throws -> RawVerifiedId
+    func send(requestContent: RawRequestContent) async throws -> RawVerifiedId
 }
