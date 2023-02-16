f8373c37720ab1c9c0b1e560e608d13f69fa795e
Sydney Morton <symorton@microsoft.com>
2023-02-16 15:27:58 -0800

    Update ContractIssuanceRequest.swift

--- WalletLibrary/WalletLibrary/Requests/RequestProtocols/Manifest/ContractIssuanceRequest.swift
+++ WalletLibrary/WalletLibrary/Requests/RequestProtocols/Manifest/ContractIssuanceRequest.swift
@@ -18,7 +18,7 @@ class ContractIssuanceRequest: VerifiedIdIssuanceRequest {
     
     public let rootOfTrust: RootOfTrust
     
-    private let verifiedIdRequester: VerifiableCredentialRequester
+    private let verifiableCredentialRequester: VerifiableCredentialRequester
     
     private let configuration: LibraryConfiguration
     
@@ -26,13 +26,13 @@ class ContractIssuanceRequest: VerifiedIdIssuanceRequest {
     
     init(content: VerifiedIdRequestContent,
          issuanceResponseContainer: IssuanceResponseContainer,
-         verifiedIdRequester: VerifiableCredentialRequester,
+         verifiableCredentialRequester: VerifiableCredentialRequester,
          configuration: LibraryConfiguration) {
         self.style = content.style
         self.requirement = content.requirement
         self.rootOfTrust = content.rootOfTrust
         self.responseContainer = issuanceResponseContainer
-        self.verifiedIdRequester = verifiedIdRequester
+        self.verifiableCredentialRequester = verifiableCredentialRequester
         self.configuration = configuration
     }
     
@@ -44,7 +44,7 @@ class ContractIssuanceRequest: VerifiedIdIssuanceRequest {
     public func complete() async -> Result<VerifiedId, Error> {
         do {
             try self.responseContainer.add(requirement: requirement)
-            let verifiableCredential = try await verifiedIdRequester.send(request: responseContainer)
+            let verifiableCredential = try await verifiableCredentialRequester.send(request: responseContainer)
             let verifiedId: VerifiedId = try configuration.mapper.map(verifiableCredential)
             return Result.success(verifiedId)
         } catch {
