44756e9884b78ac1f928ffe8c6224af4cd8edb51
Sydney Morton <symorton@microsoft.com>
2023-02-22 13:36:48 -0800

    Support id token hint flow in presentation request mapping.

--- WalletLibrary/WalletLibrary/Extensions/Mappings/VCSDK/Presentation/PresentationRequest+Mappable.swift
+++ WalletLibrary/WalletLibrary/Extensions/Mappings/VCSDK/Presentation/PresentationRequest+Mappable.swift
@@ -26,14 +26,44 @@ extension VCEntities.PresentationRequest: Mappable {
         
         let requirement = try mapper.map(presentationDefinition)
         let rootOfTrust = try mapper.map(linkedDomainResult)
+        let injectedIdToken = createInjectedIdTokenIfExists()
         
         let clientName = content.registration?.clientName ?? ""
         let requesterStyle = OpenIdVerifierStyle(name: clientName)
         
         let content = VerifiedIdRequestContent(style: requesterStyle,
                                                requirement: requirement,
-                                               rootOfTrust: rootOfTrust)
+                                               rootOfTrust: rootOfTrust,
+                                               injectedIdToken: injectedIdToken)
         
         return content
     }
+    
+    private func createInjectedIdTokenIfExists() -> InjectedIdToken? {
+        if let idTokenHint = content.idTokenHint {
+            return InjectedIdToken(rawToken: idTokenHint,
+                                              pin: createPinRequirementIfExists())
+        }
+        
+        return nil
+    }
+    
+    private func createPinRequirementIfExists() -> PinRequirement? {
+        if let pin = content.pin,
+           let type = pin.type {
+            let pinRequirement = PinRequirement(required: true,
+                                                length: pin.length,
+                                                type: type,
+                                                salt: pin.salt)
+            return pinRequirement
+        }
+        
+        return nil
+    }
+}
+
+struct InjectedIdToken {
+    let rawToken: String
+    
+    let pin: PinRequirement?
 }
