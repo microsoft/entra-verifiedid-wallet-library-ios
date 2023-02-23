c49fb18d2d4bca05df1ba3f144924601037fd448
Sydney Morton <symorton@microsoft.com>
2023-02-22 16:17:46 -0800

    Implement mapping for VerifiableCredential.

--- WalletLibrary/WalletLibrary/VerifiedId/VerifiableCredential/VerifiableCredential.swift
+++ WalletLibrary/WalletLibrary/VerifiedId/VerifiableCredential/VerifiableCredential.swift
@@ -5,6 +5,10 @@
 
 import VCEntities
 
+enum VerifiableCredentialMappingError: Error {
+    case missingJtiInVerifiableCredential
+}
+
 /**
  * Verifiable Credential object contains the raw VC, and the contract that created the Verifiable Credential.
  * This object conforms to the Mappable protocol and maps VC claims and display contract to a Verified Id.
@@ -21,11 +25,36 @@ struct VerifiableCredential: Mappable {
     }
     
     func map(using mapper: Mapping) throws -> VerifiedId {
-        return VerifiedId(id: "id",
-                          type: raw.content.vc?.type?.first ?? "",
-                          claims: [],
-                          expiresOn: Date(timeIntervalSince1970: raw.content.iat ?? 0),
-                          issuedOn: Date(timeIntervalSince1970: raw.content.exp ?? 0),
-                          raw: raw.rawValue ?? "")
+        
+        let rawToken = try raw.serialize()
+        let id = try getRequiredProperty(property: raw.content.jti, propertyName: "jti")
+        let issuedOnDate = try getRequiredProperty(property: raw.content.iat, propertyName: "issued on date")
+        let expiresOnDate = try getRequiredProperty(property: raw.content.exp, propertyName: "expires on date")
+        
+        return VerifiedId(id: id,
+                          type: VerifiedIdType.verifiableCredential,
+                          claims: try createClaims(),
+                          expiresOn: Date(timeIntervalSince1970: expiresOnDate),
+                          issuedOn: Date(timeIntervalSince1970: issuedOnDate),
+                          raw: rawToken)
+    }
+    
+    private func createClaims() throws -> [VerifiedIdClaim] {
+        let vcClaims = try getRequiredProperty(property: raw.content.vc?.credentialSubject, propertyName: "claims")
+        let claimLabels = contract.display.claims
+        
+        var verifiedIdClaims: [VerifiedIdClaim] = []
+        /// TODO: add casting to correct type from contract.
+        for (claim, value) in vcClaims {
+            if let claimStyle = claimLabels["vc.credentialSubject.\(claim)"] {
+                verifiedIdClaims.append(VerifiedIdClaim(id: claimStyle.label,
+                                                        value: value))
+            } else {
+                verifiedIdClaims.append(VerifiedIdClaim(id: claim,
+                                                        value: value))
+            }
+        }
+        
+        return verifiedIdClaims
     }
 }
