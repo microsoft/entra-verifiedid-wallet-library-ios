0fbdd8eaefba47c05bd51e74fed96b89a4cf8ff7
Sydney Morton <symorton@microsoft.com>
2023-02-24 16:45:45 -0800

    Update VerifiedIdRequestContent.swift

--- WalletLibrary/WalletLibrary/Requests/VerifiedIdRequestContent.swift
+++ WalletLibrary/WalletLibrary/Requests/VerifiedIdRequestContent.swift
@@ -6,7 +6,8 @@
 /**
  * Contents in a Verified Id Request.
  * This object is used to map protocol specific requests to common request object.
- * TODO: make this object extensibility to be able to add VerifiedIdStyle.
+ * TODO: make this object extensible by separating Presentation with Issuance esp. for InjectedIdToken logic.
+ * TODO: add VerifiedIdStyle to Issuance Content.
  */
 struct VerifiedIdRequestContent {
     
@@ -26,5 +27,34 @@ struct VerifiedIdRequestContent {
         self.requirement = requirement
         self.rootOfTrust = rootOfTrust
         self.injectedIdToken = injectedIdToken
+    }
+    
+    mutating func addRequirement(from injectedIdToken: InjectedIdToken) {
+        switch (requirement) {
+        case var groupRequirement as GroupRequirement:
+            repopulateGroupRequirementIfInjectedIdTokenExists(injectedIdToken: injectedIdToken,
+                                                              groupRequirement: &groupRequirement)
+        case var idTokenRequirement as IdTokenRequirement:
+            idTokenRequirement.fulfill(with: injectedIdToken.rawToken)
+            if let pinRequirement = injectedIdToken.pin {
+                requirement = GroupRequirement(required: false,
+                                               requirements: [idTokenRequirement, pinRequirement],
+                                               requirementOperator: .ALL)
+            }
+        default:
+            return
+        }
+    }
+    
+    private func repopulateGroupRequirementIfInjectedIdTokenExists(injectedIdToken: InjectedIdToken,
+                                                                   groupRequirement: inout GroupRequirement) {
+        for requirement in groupRequirement.requirements {
+            if var idTokenRequirement = requirement as? IdTokenRequirement {
+                idTokenRequirement.fulfill(with: injectedIdToken.rawToken)
+                if let pinRequirement = injectedIdToken.pin {
+                    groupRequirement.requirements.append(pinRequirement)
+                }
+            }
+        }
     }
 }
