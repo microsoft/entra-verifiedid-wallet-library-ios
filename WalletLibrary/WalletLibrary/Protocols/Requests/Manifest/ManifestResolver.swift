3cdcdc72fe7f7474a155c1957159d36e9294a5c3
Sydney Morton <symorton@microsoft.com>
2023-02-28 11:47:57 -0800

    Update ManifestResolver.swift

--- WalletLibrary/WalletLibrary/Protocols/Requests/Manifest/ManifestResolver.swift
+++ WalletLibrary/WalletLibrary/Protocols/Requests/Manifest/ManifestResolver.swift
@@ -3,8 +3,6 @@
 *  Licensed under the MIT License. See License.txt in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
-import VCEntities
-
 /**
  * Protocol defines the behavior of taking a url and resolving a manifest.
  * For example, it is used as a wrapper to wrap the VC SDK get contract method.
