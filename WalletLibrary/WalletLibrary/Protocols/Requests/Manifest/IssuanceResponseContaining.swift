152961158f880286e5545bed05fc452f1544f083
Sydney Morton <symorton@microsoft.com>
2023-02-28 11:47:54 -0800

    Create IssuanceResponseContaining.swift

---
+++ WalletLibrary/WalletLibrary/Protocols/Requests/Manifest/IssuanceResponseContaining.swift
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+/**
+ * Protocol defines the behavior of collecting requirements to send to requester.
+ * For example, it is used as a wrapper to wrap the VC SDK get contract method.
+ */
+protocol IssuanceResponseContaining {
+    mutating func add(requirement: Requirement) throws
+}
