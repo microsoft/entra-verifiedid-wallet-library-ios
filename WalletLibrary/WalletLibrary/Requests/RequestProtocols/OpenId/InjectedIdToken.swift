0bd2bc8dba6a79bc7388f854c13bd2b54d8b7c58
Sydney Morton <symorton@microsoft.com>
2023-02-28 11:03:04 -0800

    Update InjectedIdToken.swift

--- WalletLibrary/WalletLibrary/Requests/RequestProtocols/OpenId/InjectedIdToken.swift
+++ WalletLibrary/WalletLibrary/Requests/RequestProtocols/OpenId/InjectedIdToken.swift
@@ -4,19 +4,10 @@
 *--------------------------------------------------------------------------------------------*/
 
 /**
-<<<<<<<< HEAD:WalletLibrary/WalletLibrary/Requests/RequestProtocols/OpenId/InjectedIdToken.swift
  * An object that represents an id token that is injected from a presentation request into an issuance request.
  */
 struct InjectedIdToken {
     let rawToken: String
     
     let pin: PinRequirement?
-========
- * Protocol defines the behavior of taking a url and resolving a manifest.
- * For example, it is used as a wrapper to wrap the VC SDK get contract method.
- */
-protocol ManifestResolver {
-    /// Fetches and validates the manifest.
-    func resolve(with url: URL) async throws -> any RawManifest
->>>>>>>> dev:WalletLibrary/WalletLibrary/Protocols/Requests/Manifest/ManifestResolver.swift
 }
