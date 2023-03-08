9230730de294d55289529fe6bbff2148ef072967
Sydney Morton <symorton@microsoft.com>
2023-03-08 15:28:02 -0500

    Update OpenIdForVCResponder.swift

--- WalletLibrary/WalletLibrary/Protocols/Requests/OpenIdForVC/OpenIdForVCResponder.swift
+++ WalletLibrary/WalletLibrary/Protocols/Requests/OpenIdForVC/OpenIdForVCResponder.swift
@@ -3,13 +3,11 @@
 *  Licensed under the MIT License. See License.txt in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
-import VCEntities
-
 /**
  * Protocol is used as a wrapper to wrap the VC SDK send presentation response method.
  */
 protocol OpenIdForVCResponder {
     /// Sends the presentation response container and if successful, returns void,
     /// If unsuccessful, throws an error.
-    func send(response: PresentationResponseContainer) async throws -> Void
+    func send(response: PresentationResponse) async throws -> Void
 }
