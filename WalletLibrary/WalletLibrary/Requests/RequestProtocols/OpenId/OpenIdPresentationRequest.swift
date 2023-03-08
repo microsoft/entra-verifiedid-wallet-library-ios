a320735af89420cf62b73592840d47fdb49eb882
Sydney Morton <symorton@microsoft.com>
2023-03-08 15:29:33 -0500

    Implement complete method for openIdPresentationRequest.

--- WalletLibrary/WalletLibrary/Requests/RequestProtocols/OpenId/OpenIdPresentationRequest.swift
+++ WalletLibrary/WalletLibrary/Requests/RequestProtocols/OpenId/OpenIdPresentationRequest.swift
@@ -3,6 +3,8 @@
 *  Licensed under the MIT License. See License.txt in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
+import VCEntities
+
 /**
  * Presentation Requst that is Open Id specific.
  * TODO: we will need open id specific data to implement complete and cancel.
@@ -15,12 +17,25 @@ class OpenIdPresentationRequest: VerifiedIdPresentationRequest {
     
     let rootOfTrust: RootOfTrust
     
+    private let raw: any OpenIdRawRequest
+    
+    private let responder: OpenIdForVCResponder
+    
     private let configuration: LibraryConfiguration
     
-    init(content: VerifiedIdRequestContent, configuration: LibraryConfiguration) {
+    init(content: PresentationRequestContent,
+         openIdResponder: OpenIdForVCResponder,
+         configuration: LibraryConfiguration) throws {
+        
+        guard let raw = content.raw as? any OpenIdRawRequest else {
+            throw VerifiedIdClientError.TODO(message: "test")
+        }
+        
         self.style = content.style
         self.requirement = content.requirement
         self.rootOfTrust = content.rootOfTrust
+        self.raw = raw
+        self.responder = openIdResponder
         self.configuration = configuration
     }
     
@@ -30,7 +45,15 @@ class OpenIdPresentationRequest: VerifiedIdPresentationRequest {
     }
     
     func complete() async -> Result<(), Error> {
-        return Result.failure(VerifiedIdClientError.TODO(message: "implement"))
+        do {
+            
+            var response = try PresentationResponseContainer(from: raw)
+            try response.add(requirement: requirement)
+            try await responder.send(response: response)
+            return Result.success(())
+        } catch {
+            return Result.failure(error)
+        }
     }
     
     func cancel(message: String?) -> Result<Void, Error> {
