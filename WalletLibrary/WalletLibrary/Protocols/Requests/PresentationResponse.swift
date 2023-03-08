0f2b258fe3cd6df507a251d59d0ea4a28a27c58f
Sydney Morton <symorton@microsoft.com>
2023-03-08 16:03:20 -0500

    Create PresentationResponse.swift

---
+++ WalletLibrary/WalletLibrary/Protocols/Requests/PresentationResponse.swift
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+/**
+ * An object that describes a raw presense response and defines the behavior of adding a requirement to it.
+ * For example, a VCSDK.PresentationResponseContainer conforms to this protocol.
+ */
+protocol PresentationResponse {
+    mutating func add(requirement: Requirement) throws
+}
