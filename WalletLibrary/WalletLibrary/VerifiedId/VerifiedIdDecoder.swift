a38d2d356018ac019a136f3b120af6010e9addb0
Sydney Morton <symorton@microsoft.com>
2023-03-20 15:42:45 -0700

    Create a VerifiedIdDecoder

---
+++ WalletLibrary/WalletLibrary/VerifiedId/VerifiedIdDecoder.swift
@@ -0,0 +1,22 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+/**
+ * The Verified Id Decoder 
+ */
+public struct VerifiedIdDecoder {
+
+    private let jsonDecoder = JSONDecoder()
+
+    public init() {}
+
+    public func decode(from data: Data) throws -> any VerifiedId {
+        let encodedVerifiedId = try jsonDecoder.decode(EncodedVerifiedId.self, from: data)
+        switch encodedVerifiedId.type {
+        case .VerifiableCredential:
+            return try jsonDecoder.decode(VerifiableCredential.self, from: encodedVerifiedId.raw)
+        }
+    }
+}
