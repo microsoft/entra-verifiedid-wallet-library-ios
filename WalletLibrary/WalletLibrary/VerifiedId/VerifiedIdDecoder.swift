11a93b61bb740d4c55c9f13ebd19893a51dfd457
Sydney Morton <symorton@microsoft.com>
2023-03-21 07:52:55 -0700

    Add VerifiedIdEncoder.

--- WalletLibrary/WalletLibrary/VerifiedId/VerifiedIdDecoder.swift
+++ WalletLibrary/WalletLibrary/VerifiedId/VerifiedIdDecoder.swift
@@ -8,24 +8,21 @@ enum VerifiedIdDecoderError: Error {
     case unableToDecodeVerifiedId
     case unableToDecodeVerifiedIdRawData
 }
+
 /**
  * The Verified Id Decoder 
  */
-public struct VerifiedIdDecoder {
+struct VerifiedIdDecoder {
 
     private let jsonDecoder = JSONDecoder()
     
     private let supportedVerifiedIdTypes: [String: VerifiedId.Type]
 
-    public init() {
+    init() {
         self.supportedVerifiedIdTypes = [SupportedVerifiedIdType.VerifiableCredential.rawValue: VerifiableCredential.self]
     }
-    
-    init(supportedVerifiedIdTypes: [String: VerifiedId.Type]) {
-        self.supportedVerifiedIdTypes = supportedVerifiedIdTypes
-    }
 
-    public func decode(from data: Data) throws -> any VerifiedId {
+    func decode(from data: Data) throws -> any VerifiedId {
         do {
             let encodedVerifiedId = try jsonDecoder.decode(EncodedVerifiedId.self, from: data)
             for (key,value) in supportedVerifiedIdTypes {
