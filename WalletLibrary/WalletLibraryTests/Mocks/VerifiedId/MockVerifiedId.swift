7f12d0f3fd4d75862268edcdeafdb2e99035db17
Sydney Morton <symorton@microsoft.com>
2023-03-27 14:06:04 -0700

    Fix mocks.

--- WalletLibrary/WalletLibraryTests/Mocks/VerifiedId/MockVerifiedId.swift
+++ WalletLibrary/WalletLibraryTests/Mocks/VerifiedId/MockVerifiedId.swift
@@ -15,7 +15,45 @@ struct MockVerifiedId: VerifiedId, Equatable {
     
     var issuedOn: Date
     
+    var style: VerifiedIdStyle
+    
+    enum CodingKeys: String, CodingKey {
+        case id, issuedOn, expiresOn, double
+        case style
+    }
+    
+    init(id: String,
+         double: Double? = nil,
+         expiresOn: Date? = nil,
+         issuedOn: Date,
+         style: VerifiedIdStyle = MockVerifiedIdStyle()) {
+        self.id = id
+        self.double = double
+        self.expiresOn = expiresOn
+        self.issuedOn = issuedOn
+        self.style = style
+    }
+    
     func getClaims() -> [VerifiedIdClaim] {
         return []
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        let id = try values.decode(String.self, forKey: .id)
+        let double = try values.decode(Double.self, forKey: .double)
+        let style = try values.decode(MockVerifiedIdStyle.self, forKey: .style)
+        self.init(id: id, double: double, issuedOn: Date(), style: style)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(double, forKey: .double)
+        try container.encode(style as! MockVerifiedIdStyle, forKey: .style)
+    }
+    
+    static func == (lhs: MockVerifiedId, rhs: MockVerifiedId) -> Bool {
+        return lhs.id == rhs.id
     }
 }
