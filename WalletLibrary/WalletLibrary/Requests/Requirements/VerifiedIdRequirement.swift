c576179fa0a0c87cd906b7d10beefb25675fcebd
Sydney Morton <symorton@microsoft.com>
2023-04-19 13:48:09 -0700

    Remove unused error from Requirement.

--- WalletLibrary/WalletLibrary/Requests/Requirements/VerifiedIdRequirement.swift
+++ WalletLibrary/WalletLibrary/Requests/Requirements/VerifiedIdRequirement.swift
@@ -4,7 +4,6 @@
 *--------------------------------------------------------------------------------------------*/
 
 enum VerifiedIdRequirementError: Error {
-    case verifiedIdDoesNotMeetConstraints
     case requirementHasNotBeenFulfilled
 }
 
