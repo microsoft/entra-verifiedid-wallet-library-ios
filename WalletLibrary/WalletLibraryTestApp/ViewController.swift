c5219c8f591c4d390b517e37c560fd2077afea37
Sydney Morton <symorton@microsoft.com>
2023-04-21 08:20:23 -0700

    Add a Wallet Library Test App for tests.

---
+++ WalletLibrary/WalletLibraryTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WalletLibraryTestApp
+//
+//  Created by Sydney Morton on 4/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
