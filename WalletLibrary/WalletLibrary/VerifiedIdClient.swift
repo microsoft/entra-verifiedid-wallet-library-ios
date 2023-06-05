9a50360c07b12d0f67d9b502505143e9764cb3ee
Sydney Morton <symorton@microsoft.com>
2023-06-05 12:02:18 -0700

    Update create request to use Result helper method.

--- WalletLibrary/WalletLibrary/VerifiedIdClient.swift
+++ WalletLibrary/WalletLibrary/VerifiedIdClient.swift
@@ -24,34 +24,31 @@ public class VerifiedIdClient {
     
     /// Creates either an issuance or presentation request from the input.
     public func createRequest(from input: VerifiedIdRequestInput) async -> VerifiedIdResult<any VerifiedIdRequest> {
-        do {
-            let resolver = try requestResolverFactory.getResolver(from: input)
+        return await VerifiedIdResult<VerifiedId>.getResult {
+            let resolver = try self.requestResolverFactory.getResolver(from: input)
             let rawRequest = try await resolver.resolve(input: input)
-            let handler = try requestHandlerFactory.getHandler(from: resolver)
-            let request = try await handler.handleRequest(from: rawRequest)
-            return Result.success(request)
-        } catch let error as VerifiedIdError {
-            return error.result()
-        } catch {
-            return VerifiedIdErrors.UnspecifiedError(error: error).result()
+            let handler = try self.requestHandlerFactory.getHandler(from: resolver)
+            return try await handler.handleRequest(from: rawRequest)
         }
     }
     
     /// Encode a VerifiedId into Data.
+    /// TODO: create a VerifiedIdError for encoding.
     public func encode(verifiedId: VerifiedId) -> VerifiedIdResult<Data> {
         do {
             let encodedVerifiedId = try configuration.verifiedIdEncoder.encode(verifiedId: verifiedId)
-            return Result.success(encodedVerifiedId)
+            return VerifiedIdResult.success(encodedVerifiedId)
         } catch {
             return VerifiedIdErrors.UnspecifiedError(error: error).result()
         }
     }
     
     /// Decode raw Data and return a VerifiedId.
+    /// TODO: create a VerifiedIdError for decoding.
     public func decodeVerifiedId(from raw: Data) -> VerifiedIdResult<VerifiedId> {
         do {
             let verifiedId = try configuration.verifiedIdDecoder.decode(from: raw)
-            return Result.success(verifiedId)
+            return VerifiedIdResult.success(verifiedId)
         } catch {
             return VerifiedIdErrors.UnspecifiedError(error: error).result()
         }
