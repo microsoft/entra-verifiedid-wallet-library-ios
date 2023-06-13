8bd7ceb3772062769b476f7149752f8a630f1e3d
Sydney Morton <symorton@microsoft.com>
2023-06-13 17:13:27 -0400

    Add comments to VerifiedIdCorrelationHeader

--- WalletLibrary/WalletLibrary/Protocols/Utilities/VerifiedIdCorrelationHeader.swift
+++ WalletLibrary/WalletLibrary/Protocols/Utilities/VerifiedIdCorrelationHeader.swift
@@ -3,14 +3,20 @@
 //
 
 /**
- * Protocol for adding Correlation Headers to network calls.
+ * This protocol defines an object that can be used to inject correlation headers into all the network calls in the library.
+ * The value will be reset at the start of each flow and each correlation header will be updated for each network call made
+ * through out the flow.
+ *
+ * To use this feature, create a class that conforms to the protocol. A library such as the `CorrelationVector` Pod
+ * by Microsoft (https://github.com/microsoft/CorrelationVector) can be used to help with the implementation.
+ * Then inject an instance of the class into the `VerifiedIdClient` using the `VerifiedIdClientBuilder`.
  */
 public protocol VerifiedIdCorrelationHeader {
     
     /// The name of the header (i.e. ms-cv).
     var name: String { get }
     
-    /// The value of the header (i.e. some random UUID).
+    /// The value of the header (i.e. some random UUID defined by Correlation Vector v2.1).
     var value: String { get }
     
     /// Updates the Correlation Header Value before each network call.
