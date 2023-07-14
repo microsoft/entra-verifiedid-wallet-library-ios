fd3efb417865d4b45740600b5d47e4cda7ec46c3
Sydney Morton <symorton@microsoft.com>
2023-07-14 09:21:25 -0700

    Update LinkedDomainService.

--- WalletLibrary/Submodules/VerifiableCredential-SDK-iOS/VCServices/VCServicesTests/LinkedDomainServiceTests.swift
+++ WalletLibrary/Submodules/VerifiableCredential-SDK-iOS/VCServices/VCServicesTests/LinkedDomainServiceTests.swift
@@ -12,92 +12,64 @@ class LinkedDomainServiceTests: XCTestCase {
     let mockDomainUrl = "testDomain.com"
     
     override func setUpWithError() throws {
-        
         service = setUpService()
-        
         MockDiscoveryApiCalls.wasGetCalled = false
         MockWellKnownConfigDocumentApiCalls.wasGetCalled = false
         MockDomainLinkageCredentialValidator.wasValidateCalled = false
     }
     
-    func testLinkedDomainVerifiedResult() {
+    func testLinkedDomainVerifiedResult() async throws {
+        // Arrange
+        let document = createIdentifierDocument(serviceEndpointType: ServicesConstants.LINKED_DOMAINS_SERVICE_ENDPOINT_TYPE)
         
-        let expec = self.expectation(description: "Fire")
-        service.validateLinkedDomain(from: validDid).done {
-            result in
-            print(result)
-            XCTAssertEqual(result, LinkedDomainResult.linkedDomainVerified(domainUrl: self.mockDomainUrl))
-            expec.fulfill()
-        }.catch { error in
-            print(error)
-            XCTFail()
-            expec.fulfill()
-        }
+        // Act
+        let result = try await service.validateLinkedDomain(from: document)
         
-        wait(for: [expec], timeout: 5)
+        // Assert
+        XCTAssertEqual(result, LinkedDomainResult.linkedDomainVerified(domainUrl: self.mockDomainUrl))
     }
     
-    func testLinkedDomainMissingResult() {
-        let expec = self.expectation(description: "Fire")
+    func testLinkedDomainMissingResult() async throws {
+        // Arrange
+        let document = createIdentifierDocument(serviceEndpointType: "Wrong Service Type")
         
-        let service = setUpService(serviceEndpointType: "WrongServiceType")
+        // Act
+        let result = try await service.validateLinkedDomain(from: document)
         
-        service.validateLinkedDomain(from: validDid).done {
-            result in
-            print(result)
-            XCTAssertEqual(result, LinkedDomainResult.linkedDomainMissing)
-            expec.fulfill()
-        }.catch { error in
-            print(error)
-            XCTFail()
-            expec.fulfill()
-        }
-        
-        wait(for: [expec], timeout: 5)
+        // Assert
+        XCTAssertEqual(result, LinkedDomainResult.linkedDomainMissing)
     }
     
-    func testLinkedDomainUnverifiedResult() {
-        let expec = self.expectation(description: "Fire")
+    func testLinkedDomainUnverifiedResult() async throws {
+        // Arrange
+        let document = createIdentifierDocument(serviceEndpointType: ServicesConstants.LINKED_DOMAINS_SERVICE_ENDPOINT_TYPE)
+        let service = setUpService(isLinkageCredentialValid: false)
         
-        let service = setUpService(isValid: false)
+        // Act
+        let result = try await service.validateLinkedDomain(from: document)
         
-        service.validateLinkedDomain(from: validDid).done {
-            result in
-            print(result)
-            XCTAssertEqual(result, LinkedDomainResult.linkedDomainUnverified(domainUrl: self.mockDomainUrl))
-            expec.fulfill()
-        }.catch { error in
-            print(error)
-            XCTFail()
-            expec.fulfill()
-        }
-        
-        wait(for: [expec], timeout: 5)
+        // Assert
+        XCTAssertEqual(result, LinkedDomainResult.linkedDomainUnverified(domainUrl: self.mockDomainUrl))
     }
     
-    private func setUpService(serviceEndpointType: String = ServicesConstants.LINKED_DOMAINS_SERVICE_ENDPOINT_TYPE,
-                              isValid: Bool = true) -> LinkedDomainService {
+    private func setUpService(isLinkageCredentialValid: Bool = true) -> LinkedDomainService {
+        let wellKnownApiCalls = MockWellKnownConfigDocumentApiCalls(resolveSuccessfully: true)
+        let validator = MockDomainLinkageCredentialValidator(isValid: isLinkageCredentialValid)
         
+        return LinkedDomainService(wellKnownDocumentApiCalls: wellKnownApiCalls,
+                                   domainLinkageValidator: validator)
+    }
+    
+    private func createIdentifierDocument(serviceEndpointType: String) -> IdentifierDocument {
         let endpoint = IdentifierDocumentServiceEndpoint(origins: [mockDomainUrl])
         
         let serviceEndpointDescriptor = IdentifierDocumentServiceEndpointDescriptor(id: "testServiceEndpoint",
-                                                                type: serviceEndpointType,
-                                                                serviceEndpoint: endpoint)
+                                                                                    type: serviceEndpointType,
+                                                                                    serviceEndpoint: endpoint)
         
-        let document = IdentifierDocument(service: [serviceEndpointDescriptor],
-                                          verificationMethod: [],
-                                          authentication: [],
-                                          id: validDid)
-        
-        let discoveryApiCalls = MockDiscoveryApiCalls(resolveSuccessfully: true,
-                                                      withIdentifierDocument: document)
-        let wellKnownApiCalls = MockWellKnownConfigDocumentApiCalls(resolveSuccessfully: true)
-        let validator = MockDomainLinkageCredentialValidator(isValid: isValid)
-        
-        return LinkedDomainService(didDocumentDiscoveryApiCalls: discoveryApiCalls,
-                                      wellKnownDocumentApiCalls: wellKnownApiCalls,
-                                      domainLinkageValidator: validator)
+        return IdentifierDocument(service: [serviceEndpointDescriptor],
+                                  verificationMethod: [],
+                                  authentication: [],
+                                  id: "did:test:1234")
     }
-    
-    let validDid = "did:ion:EiD89iut8PMhRz5zSlPl_zkHgEfRBiA9zdXYwAdrk1_JYg:eyJkZWx0YSI6eyJwYXRjaGVzIjpbeyJhY3Rpb24iOiJyZXBsYWNlIiwiZG9jdW1lbnQiOnsicHVibGljX2tleXMiOlt7ImlkIjoic2lnX2FkODRkZjIzIiwiandrIjp7ImNydiI6InNlY3AyNTZrMSIsImt0eSI6IkVDIiwieCI6IllMM2xlNC12YlNsa2ZINnJoQWJlbEJXTDByVlBieVJYTGtIUmx3REFZOTQiLCJ5IjoiYWFDZG1idjF0R1htZU1QWUdUY0ZqLUFnc2d2c2RNLXlGR2tnVU1QX04tVSJ9LCJwdXJwb3NlIjpbImF1dGgiLCJnZW5lcmFsIl0sInR5cGUiOiJFY2RzYVNlY3AyNTZrMVZlcmlmaWNhdGlvbktleTIwMTkifV0sInNlcnZpY2VfZW5kcG9pbnRzIjpbeyJlbmRwb2ludCI6eyJvcmlnaW5zIjpbImh0dHBzOi8vY29udG9zb3VuaXZlcnNpdHktZGlkZGVtby5henVyZXdlYnNpdGVzLm5ldC8iXX0sImlkIjoibGlua2VkZG9tYWlucyIsInR5cGUiOiJMaW5rZWREb21haW5zIn1dfX1dLCJ1cGRhdGVfY29tbWl0bWVudCI6IkVpQS1HSFJRVkcxV2F0bGprSVQxOGxvbWZXbTY0N2QwYmtlRmpCR2tMUjZMRXcifSwic3VmZml4X2RhdGEiOnsiZGVsdGFfaGFzaCI6IkVpQXBta0dpa2RiX3pHb1JrVU5vbmd0TXJlZS1xTjhPeEtsVmJvYUk5ZG9DdXciLCJyZWNvdmVyeV9jb21taXRtZW50IjoiRWlCb19vVkJYSEJmZGhfRFpkZjlqU3ZwQ1lnRmNkcXc3cDZFa1pPanU3bUREQSJ9fQ"
 }
