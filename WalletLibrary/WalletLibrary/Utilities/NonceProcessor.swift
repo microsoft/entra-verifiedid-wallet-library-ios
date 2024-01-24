1c55ab6434a208752861c304cc41a7e3c2504281
Sydney Morton <symorton@microsoft.com>
2024-01-24 11:47:51 -0800

    Make Nonce Processor Internal.

--- WalletLibrary/WalletLibrary/Utilities/NonceProcessor.swift
+++ WalletLibrary/WalletLibrary/Utilities/NonceProcessor.swift
@@ -3,61 +3,59 @@
 *  Licensed under the MIT License. See License.txt in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
-enum NonceProcessorError: Error {
-    case unableToSerializeDid
+enum NonceProcessorError: Error 
+{
+    case UnableToSerializeDid
 }
 
-public struct NonceProcessor {
-    
-    public init() {}
-
-    public func createNonce(from identifierManager: IdentifierManager? = nil) -> String? {
-        do {
-            let identifierManager = identifierManager ?? IdentifierService()
-            let didHash = try getDidHash(from: identifierManager.fetchOrCreateMasterIdentifier().longFormDid)
-
-            guard let prefix = createRandomURLSafeString(withSize: 32) else {
+struct NonceProcessor 
+{
+    func createNonce(fromIdentifier did: String) -> String? 
+    {
+        do 
+        {
+            let didHash = try getDidHash(from: did)
+            guard let prefix = createRandomURLSafeString(withSize: 32) else 
+            {
                 return nil
             }
 
             return "\(prefix).\(didHash)"
 
-        } catch {
+        } 
+        catch
+        {
             return nil
         }
     }
 
-    private func getDidHash(from did: String) throws -> String {
+    private func getDidHash(from did: String) throws -> String 
+    {
 
-        guard let serialized = did.data(using: .utf8) else {
-            throw NonceProcessorError.unableToSerializeDid
+        guard let serialized = did.data(using: .utf8) else 
+        {
+            throw NonceProcessorError.UnableToSerializeDid
         }
 
         let sha = Sha512()
         let hash = sha.hash(data: serialized)
-        return encodeBase64urlNoPadding(hash)
+        return hash.base64URLEncodedWithNoPaddingString()
     }
 
-    private func createRandomURLSafeString(withSize size: Int) -> String? {
-
-        guard let randomData = NSMutableData(length: size) else {
+    private func createRandomURLSafeString(withSize size: Int) -> String? 
+    {
+        guard let randomData = NSMutableData(length: size) else 
+        {
             return nil
         }
 
         let result = SecRandomCopyBytes(kSecRandomDefault, randomData.length, randomData.mutableBytes)
 
-        if result != 0 {
+        if result != 0 
+        {
             return nil
         }
 
-        return encodeBase64urlNoPadding(randomData as Data)
-    }
-
-    private func encodeBase64urlNoPadding(_ data: Data) -> String {
-        var encodedHash = data.base64EncodedString()
-        encodedHash = encodedHash.replacingOccurrences(of: "+", with: "-")
-        encodedHash = encodedHash.replacingOccurrences(of: "/", with: "_")
-        encodedHash = encodedHash.replacingOccurrences(of: "=", with: "")
-        return encodedHash
+        return (randomData as Data).base64URLEncodedWithNoPaddingString()
     }
 }
