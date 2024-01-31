97f7800d7764d95a8e700af02ddba7642ef1837a
Sydney Morton <symorton@microsoft.com>
2024-01-31 11:08:18 -0800

    Update Request and Handler for OpenID

--- WalletLibrary/WalletLibrary/Requests/Resolvers/OpenIdURLRequestResolver.swift
+++ WalletLibrary/WalletLibrary/Requests/Resolvers/OpenIdURLRequestResolver.swift
@@ -3,10 +3,6 @@
 *  Licensed under the MIT License. See License.txt in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
-#if canImport(VCEntities)
-    import VCEntities
-#endif
-
 /**
  * Errors associated with the Open Id URL Request Resolver.
  */
@@ -30,16 +26,6 @@ struct OpenIdURLRequestResolver: RequestResolving {
         self.configuration = configuration
     }
     
-    /// Whether or not the request handler given request handler can handle the resolved raw request.
-    func canResolve(using handler: any RequestHandling) -> Bool {
-        
-        guard handler is OpenIdRequestHandler else {
-            return false
-        }
-        
-        return true
-    }
-    
     /// Whether or not the resolver can resolve input given.
     func canResolve(input: VerifiedIdRequestInput) -> Bool {
         
@@ -55,7 +41,7 @@ struct OpenIdURLRequestResolver: RequestResolving {
     }
     
     /// Resolve raw request from input given.
-    func resolve(input: VerifiedIdRequestInput) async throws -> any OpenIdRawRequest {
+    func resolve(input: VerifiedIdRequestInput) async throws -> Any {
         
         guard let input = input as? VerifiedIdRequestURL else {
             throw OpenIdURLRequestResolverError.unsupportedVerifiedIdRequestInputWith(type: String(describing: type(of: input)))
