5342e4e705187ca9c538d5c351a7bb99edfb5614
Sydney Morton <symorton@microsoft.com>
2024-02-01 11:42:55 -0800

    Add Tests.

---
+++ WalletLibrary/WalletLibraryTests/LibraryConfigurationTests.swift
@@ -0,0 +1,29 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+import XCTest
+@testable import WalletLibrary
+
+class LibraryConfigurationTests: XCTestCase {
+    
+    func testIsPreviewFeatureSupported_WhenFeatureIsSupported_ReturnTrue() async throws {
+        // Arrange
+        let mockFeatureFlag = "MockFeatureFlag"
+        let previewFeatureFlag = PreviewFeatureFlag(previewFeatureFlags: [mockFeatureFlag])
+        let configuration = LibraryConfiguration(previewFeatureFlags: previewFeatureFlag)
+        
+        // Act / Arrange
+        XCTAssert(configuration.isPreviewFeatureFlagSupported(mockFeatureFlag))
+    }
+    
+    func testIsPreviewFeatureSupported_WhenFeatureDoesNotExist_ReturnFalse() async throws {
+        // Arrange
+        let mockFeatureFlag = "MockFeatureFlag"
+        let configuration = LibraryConfiguration()
+        
+        // Act / Arrange
+        XCTAssertFalse(configuration.isPreviewFeatureFlagSupported(mockFeatureFlag))
+    }
+}
