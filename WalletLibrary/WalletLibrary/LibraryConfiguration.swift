4367a291dded17ca9c55fdf496083082a9f93795
Sydney Morton <symorton@microsoft.com>
2024-02-01 10:58:20 -0800

    Update LibraryConfiguration.swift

--- WalletLibrary/WalletLibrary/LibraryConfiguration.swift
+++ WalletLibrary/WalletLibrary/LibraryConfiguration.swift
@@ -20,18 +20,28 @@ class LibraryConfiguration {
     let correlationHeader: VerifiedIdCorrelationHeader?
     
     let identifierManager: IdentifierManager
+    
+    let previewFeatureFlagsSupported: [PreviewFeatureFlag]
 
     init(logger: WalletLibraryLogger,
          mapper: Mapping,
          correlationHeader: VerifiedIdCorrelationHeader? = nil,
          verifiedIdDecoder: VerifiedIdDecoding = VerifiedIdDecoder(),
          verifiedIdEncoder: VerifiedIdEncoding = VerifiedIdEncoder(),
-         identifierManager: IdentifierManager? = nil) {
+         identifierManager: IdentifierManager? = nil,
+         previewFeatureFlagsSupported: [PreviewFeatureFlag] = []) {
         self.logger = logger
         self.mapper = mapper
         self.correlationHeader = correlationHeader
         self.verifiedIdDecoder = verifiedIdDecoder
         self.verifiedIdEncoder = verifiedIdEncoder
         self.identifierManager = identifierManager ?? VerifiableCredentialSDK.identifierService
+        self.previewFeatureFlagsSupported = previewFeatureFlagsSupported
+    }
+    
+    /// Helper function to determine if a preview feature flag is supported
+    func isPreviewFeatureFlagSupported(_ featureFlag: PreviewFeatureFlag) -> Bool
+    {
+        return previewFeatureFlagsSupported.contains(featureFlag)
     }
 }
