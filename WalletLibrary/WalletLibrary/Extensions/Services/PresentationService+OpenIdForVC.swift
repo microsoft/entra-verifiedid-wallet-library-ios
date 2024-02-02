f669c18d438c33944539bb18c7700850b083110e
Sydney Morton <symorton@microsoft.com>
2024-02-02 14:50:07 -0800

    Update where validateRequest goes.

--- WalletLibrary/WalletLibrary/Extensions/Services/PresentationService+OpenIdForVC.swift
+++ WalletLibrary/WalletLibrary/Extensions/Services/PresentationService+OpenIdForVC.swift
@@ -10,7 +10,14 @@ enum PresentationServiceExtensionError: Error {
 /**
  * An extension of the VCServices.PresentationService class.
  */
-extension PresentationService: OpenIdForVCResolver, OpenIdResponder {
+extension PresentationService: OpenIdForVCResolver, OpenIdResponder 
+{
+    /// If request was already fetched, validates the serialzied request.
+    func validateRequest(data: Data) async throws -> any OpenIdRawRequest
+    {
+        let request = try PresentationRequestDecoder().decode(data: data)
+        return try await validate(request: request)
+    }
     
     /// Fetches and validates the presentation request.
     func getRequest(url: String) async throws -> any OpenIdRawRequest {
