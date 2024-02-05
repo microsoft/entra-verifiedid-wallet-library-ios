dba3cc7f69a41b90ae067803b62ddf826a346d53
Sydney Morton <symorton@microsoft.com>
2024-02-05 12:44:09 -0800

    Add extensions to LibraryNetworking protocol to allow for defaults.

--- WalletLibrary/WalletLibrary/Protocols/Networking/LibraryNetworking.swift
+++ WalletLibrary/WalletLibrary/Protocols/Networking/LibraryNetworking.swift
@@ -3,6 +3,7 @@
 *  Licensed under the MIT License. See License.txt in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
+/// A protocol for `WalletLibraryNetworking` to be used to mock the class during testing.
 protocol LibraryNetworking
 {
     func resetCorrelationHeader()
@@ -15,4 +16,26 @@ protocol LibraryNetworking
                                                      url: URL,
                                                      _ type: Operation.Type,
                                                      additionalHeaders: [String: String]?) async throws -> Operation.ResponseBody
+}
+
+/// Extend LibraryNetworking to allow for defaults in the method signatures.
+extension LibraryNetworking
+{
+    func fetch<Operation: WalletLibraryFetchOperation>(url: URL,
+                                                       _ type: Operation.Type,
+                                                       additionalHeaders: [String: String]? = nil) async throws -> Operation.ResponseBody
+    {
+        return try await self.fetch(url: url, type, additionalHeaders: additionalHeaders)
+    }
+    
+    func post<Operation: WalletLibraryPostOperation>(requestBody: Operation.RequestBody,
+                                                     url: URL,
+                                                     _ type: Operation.Type,
+                                                     additionalHeaders: [String: String]? = nil) async throws -> Operation.ResponseBody
+    {
+        return try await self.post(requestBody: requestBody,
+                                   url: url,
+                                   type,
+                                   additionalHeaders: additionalHeaders)
+    }
 }
