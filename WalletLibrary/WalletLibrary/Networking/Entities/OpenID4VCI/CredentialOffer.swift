59a5230d583c3af881babaad18515026aa9c8290
Sydney Morton <symorton@microsoft.com>
2024-02-06 16:33:46 -0800

    Update CredentialOffer.swift

--- WalletLibrary/WalletLibrary/Networking/Entities/OpenID4VCI/CredentialOffer.swift
+++ WalletLibrary/WalletLibrary/Networking/Entities/OpenID4VCI/CredentialOffer.swift
@@ -9,25 +9,80 @@
 struct CredentialOffer: Codable
 {
     /// The end point of the credential issuer metadata.
-    let credential_issuer: String?
+    let credential_issuer: String
     
     /// The state of the request. Opaque to the wallet.
-    let issuer_session: String?
+    let issuer_session: String
     
     /// The credential ids that will be used to issue the Verified ID.
-    let credential_configuration_ids: [String]?
+    let credential_configuration_ids: [String]
     
     /// An object indicating to the Wallet the Grant Types the Credential Issuer's AS is prepared to process for this Credential Offer.
-    let grants: [String: CredentialOfferGrants]?
+    let grants: [String: CredentialOfferGrant]
+    
+    enum CodingKeys: String, CodingKey
+    {
+        case credential_issuer = "credential_issuer"
+        case issuer_session
+        case credential_configuration_ids
+        case grants
+    }
 }
 
-struct CredentialOfferGrants: Codable
+struct CredentialOfferGrant: Codable
 {
     /**
-     * A string that the Wallet can use to identify the Authorization Server to use with this grant type 
+     * A string that the Wallet can use to identify the Authorization Server to use with this grant type
      * when authorization_servers parameter in the Credential Issuer metadata has multiple entries.
      * MUST NOT be used otherwise.
      * The value of this parameter MUST match with one of the values in the authorization_servers array obtained from the Credential Issuer metadata.
      */
     let authorization_server: String?
+    
+    enum CodingKeys: String, CodingKey
+    {
+        case authorization_server
+    }
+}
+
+extension CredentialOffer: MappableTarget
+{
+    static func map(_ object: Dictionary<String, Any>, using mapper: Mapping) throws -> Self
+    {
+        let credentialIssuer: String = try validateValueExists(CodingKeys.credential_issuer.rawValue,
+                                                               in: object)
+        let issuerSession: String = try validateValueExists(CodingKeys.issuer_session.rawValue, in: object)
+        let credentialConfigurationIds: [String] = try validateValueExists(CodingKeys.credential_configuration_ids.rawValue,
+                                                                           in: object)
+        let rawGrants: [String: Any] = try validateValueExists(CodingKeys.grants.rawValue,
+                                                               in: object)
+        var grants: [String: CredentialOfferGrant] = [:]
+        for (key, value) in rawGrants
+        {
+            if let rawGrant = value as? [String: Any]
+            {
+                grants[key] = try mapper.map(rawGrant, type: CredentialOfferGrant.self)
+            }
+        }
+        
+        guard grants.isEmpty else
+        {
+            throw OpenId4VCIHandlerError.CredentialOfferGrantPropertyIsEmpty
+        }
+        
+        return CredentialOffer(credential_issuer: credentialIssuer,
+                               issuer_session: issuerSession,
+                               credential_configuration_ids: credentialConfigurationIds,
+                               grants: grants)
+    }
+}
+
+extension CredentialOfferGrant: MappableTarget
+{
+    static func map(_ object: Dictionary<String, Any>, using mapper: Mapping) throws -> CredentialOfferGrant
+    {
+        let authorizationServer: String = try validateValueExists(CodingKeys.authorization_server.rawValue,
+                                                                  in: object)
+        return CredentialOfferGrant(authorization_server: authorizationServer)
+    }
 }
