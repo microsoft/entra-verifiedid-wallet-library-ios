7b8c167d2210f69f5c1d6a64c199e084c40c8bb2
Sydney Morton <symorton@microsoft.com>
2024-02-08 15:50:58 -0800

    Add mocks.

---
+++ WalletLibrary/WalletLibraryTests/Mocks/RootOfTrust/MockIdentifierDocumentResolver.swift
@@ -0,0 +1,31 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+@testable import WalletLibrary
+
+struct MockIdentifierDocumentResolver: IdentifierDocumentResolver
+{
+    enum ExpectedError: Error
+    {
+        case expectedToBeThrown
+    }
+    
+    private let mockResolve: ((String) throws -> IdentifierDocument)?
+    
+    init(mockResolve: ((String) throws -> IdentifierDocument)? = nil)
+    {
+        self.mockResolve = mockResolve
+    }
+    
+    func resolve(identifier: String) async throws -> IdentifierDocument
+    {
+        if let mockResolve = mockResolve
+        {
+            return try mockResolve(identifier)
+        }
+        
+        throw ExpectedError.expectedToBeThrown
+    }
+}
---
+++ WalletLibrary/WalletLibraryTests/Mocks/RootOfTrust/MockRootOfTrustResolver.swift
@@ -0,0 +1,31 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+@testable import WalletLibrary
+
+struct MockRootOfTrustResolver: RootOfTrustResolver
+{
+    enum ExpectedError: Error
+    {
+        case expectedToBeThrown
+    }
+    
+    private let shouldThrowError: Bool
+    
+    init(shouldThrowError: Bool = false)
+    {
+        self.shouldThrowError = shouldThrowError
+    }
+    
+    func resolve(using identifier: Any) async throws -> RootOfTrust
+    {
+        if shouldThrowError
+        {
+            throw ExpectedError.expectedToBeThrown
+        }
+        
+        return RootOfTrust(verified: true, source: "mockSource")
+    }
+}
