016af3eb69eb11022827a8f02136f431b136546a
Sydney <symorton@microsoft.com>
2024-02-08 10:10:04 -0800

    Add OpenId4VCIHandler (#69)

---
+++ WalletLibrary/WalletLibrary/Networking/Operations/CredentialMetadataFetchOperation.swift
@@ -0,0 +1,48 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+/**
+ * The Network Operation for the Credential Metadata fetch request.
+ */
+struct CredentialMetadataFetchOperation: WalletLibraryFetchOperation
+{
+    typealias ResponseBody = CredentialMetadata
+    
+    /// The decoder for the Credential Metadata.
+    struct CredentialMetadataDecoder: Decoding
+    {
+        typealias ResponseBody = CredentialMetadata
+        
+        func decode(data: Data) throws -> CredentialMetadata
+        {
+            return try JSONDecoder().decode(CredentialMetadata.self, from: data)
+        }
+    }
+    
+    var decoder = CredentialMetadataDecoder()
+    
+    var urlSession: URLSession
+    
+    var urlRequest: URLRequest
+    
+    var correlationVector: VerifiedIdCorrelationHeader?
+    
+    init(url: URL,
+         additionalHeaders: [String : String]?,
+         urlSession: URLSession,
+         correlationVector: VerifiedIdCorrelationHeader?)
+    {
+        self.urlSession = urlSession
+        self.correlationVector = correlationVector
+        self.urlRequest = URLRequest(url: url)
+        
+        /// Adds value to prefer header, appending if value already exists.
+        /// Adds value to prefer header, appending if value already exists.
+        let preferHeader = [OpenID4VCINetworkConstants.PreferHeaderField: OpenID4VCINetworkConstants.InteropProfileVersion]
+        addHeadersToURLRequest(headers: preferHeader)
+        
+        addHeadersToURLRequest(headers: additionalHeaders)
+    }
+}
---
+++ WalletLibrary/WalletLibraryTests/Requests/Handlers/OpenId4VCIHandlerTests.swift
@@ -0,0 +1,88 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+import XCTest
+@testable import WalletLibrary
+
+class OpenId4VCIHandlerTests: XCTestCase 
+{
+    func testCanHandle_WithInvalidRawRequest_ReturnsFalse() async throws
+    {
+        
+        // Arrange
+        let invalidRawRequest = "invalid raw request"
+        let configuration = LibraryConfiguration()
+        let handler = OpenId4VCIHandler(configuration: configuration)
+        
+        // Act
+        XCTAssertFalse(handler.canHandle(rawRequest: invalidRawRequest))
+    }
+    
+    func testCanHandle_WithInvalidJSONRequest_ReturnsFalse() async throws
+    {
+        
+        // Arrange
+        let invalidRawRequest = ["invalid": "request"]
+        let configuration = LibraryConfiguration()
+        let handler = OpenId4VCIHandler(configuration: configuration)
+        
+        // Act
+        XCTAssertFalse(handler.canHandle(rawRequest: invalidRawRequest))
+    }
+    
+    func testCanHandle_WithValidJSONRequest_ReturnsTrue() async throws
+    {
+        
+        // Arrange
+        let rawRequest = createJSONCredentialOffer()
+        let configuration = LibraryConfiguration()
+        let handler = OpenId4VCIHandler(configuration: configuration)
+        
+        // Act
+        XCTAssert(handler.canHandle(rawRequest: rawRequest))
+    }
+    
+    func testHandle_WithInvalidRawRequest_ThrowsError() async throws 
+    {
+        
+        // Arrange
+        let invalidRawRequest = "invalid raw request"
+        let configuration = LibraryConfiguration()
+        let handler = OpenId4VCIHandler(configuration: configuration)
+        
+        do
+        {
+            // Act
+            let _ = try await handler.handle(rawRequest: invalidRawRequest)
+        }
+        catch
+        {
+            // Assert
+            XCTAssert(error is OpenId4VCIHandlerError)
+            XCTAssertEqual(error as? OpenId4VCIHandlerError, .InputNotSupported)
+        }
+    }
+    
+    func testHandle_WithRawRequest_ReturnsVerifiedIdRequest() async throws
+    {
+        // TODO: handle successful case in next PR
+    }
+    
+    private func createJSONCredentialOffer() -> [String: Any]
+    {
+        var json: [String: Any] = [
+            "credential_issuer": "expectedCredentialIssuer",
+            "issuer_session": "expectedIssuerSession",
+            "credential_configuration_ids": ["expectedCredentialIds"],
+            "grants": [
+                "authorization_code": [
+                    "authorization_server": "expectedAuthorizationServer"
+                ]
+            ]
+        ]
+        
+        return json
+    }
+}
