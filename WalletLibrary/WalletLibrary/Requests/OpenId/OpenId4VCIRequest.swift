50c350bd7afa90eeb1db1452852960b88b9ee20a
Sydney Morton <symorton@microsoft.com>
2024-02-14 08:29:50 -0800

    Add sendIssuanceRequest to OpenId4VCIRequest.

--- WalletLibrary/WalletLibrary/Requests/OpenId/OpenId4VCIRequest.swift
+++ WalletLibrary/WalletLibrary/Requests/OpenId/OpenId4VCIRequest.swift
@@ -29,6 +29,8 @@ class OpenId4VCIRequest: VerifiedIdIssuanceRequest
     /// The credential offer for the credential being requested.
     private let credentialOffer: CredentialOffer
     
+    private let requestFormatter: OpenId4VCIRequestFormatter
+    
     /// The library configuration.
     private let configuration: LibraryConfiguration
     
@@ -47,15 +49,15 @@ class OpenId4VCIRequest: VerifiedIdIssuanceRequest
         self.credentialMetadata = credentialMetadata
         self.credentialOffer = credentialOffer
         self.configuration = configuration
+        self.requestFormatter = OpenId4VCIRequestFormatter(configuration: configuration)
     }
     
     /// Completes the issuance process, returning the result of the issuance request.
     /// - Returns: A `VerifiedIdResult` containing the issued `VerifiedId` or an `VerifiedIdError`.
     public func complete() async -> VerifiedIdResult<VerifiedId>
     {
-        let result: VerifiedIdResult<VerifiedId> = await VerifiedIdResult<VerifiedId>.getResult {
-            let errorMessage = "Not implemented."
-            throw VerifiedIdError(message: errorMessage, code: "")
+        let result = await VerifiedIdResult<VerifiedId>.getResult {
+            return try await self.sendIssuanceRequest()
         }
         
         return result
@@ -71,5 +73,114 @@ class OpenId4VCIRequest: VerifiedIdIssuanceRequest
         }
         
         return result
+    }
+    
+    private func sendIssuanceRequest() async throws -> VerifiedId
+    {
+        let rawRequest = try createRawRequest()
+        
+        guard let accessTokenRequirement = requirement as? AccessTokenRequirement,
+              let accessToken = accessTokenRequirement.accessToken else
+        {
+            throw OpenId4VCIValidationError.MalformedCredentialMetadata(message: "")
+        }
+        
+        let url = URL(string: credentialMetadata.credential_endpoint!)!
+        let test = try await configuration.networking.post(requestBody: rawRequest,
+                                                           url: url,
+                                                           OpenID4VCIPostOperation.self,
+                                                           additionalHeaders: ["Authorization": "Bearer \(accessToken)"])
+        
+        let credential = try RawOpenID4VCIResponse.getRequiredProperty(property: test.credential,
+                                                                       propertyName: "credential")
+        
+        let verifiedId = try OpenID4VCIVerifiedId(raw: credential, metadata: credentialMetadata)
+        return verifiedId
+    }
+    
+    private func createRawRequest() throws -> RawOpenID4VCIRequest
+    {
+        guard let accessTokenRequirement = requirement as? AccessTokenRequirement,
+              let accessToken = accessTokenRequirement.accessToken else
+        {
+            throw OpenId4VCIValidationError.MalformedCredentialMetadata(message: "")
+        }
+        
+        let rawRequest = try requestFormatter.format(credentialOffer: credentialOffer,
+                                                     credentialEndpoint: credentialMetadata.credential_endpoint!,
+                                                     accessToken: accessToken)
+        return rawRequest
+    }
+}
+
+struct OpenId4VCIRequestFormatter
+{
+    private let signer: TokenSigning
+    
+    private let headerFormatter = JwsHeaderFormatter()
+    
+    private let configuration: LibraryConfiguration
+    
+    init(signer: TokenSigning = Secp256k1Signer(),
+         configuration: LibraryConfiguration)
+    {
+        self.signer = signer
+        self.configuration = configuration
+    }
+    
+    func format(credentialOffer: CredentialOffer,
+                credentialEndpoint: String,
+                accessToken: String) throws -> RawOpenID4VCIRequest
+    {
+        guard let configurationId = credentialOffer.credential_configuration_ids.first else
+        {
+            throw OpenId4VCIValidationError.MalformedCredentialMetadata(message: "")
+        }
+        
+        let jwtProof = try formatProof(configurationId: configurationId,
+                                       credentialIssuer: credentialEndpoint,
+                                       accessToken: accessToken)
+        let proof = OpenID4VCIJWTProof(jwt: jwtProof)
+        let rawRequest = RawOpenID4VCIRequest(credential_identifier: configurationId,
+                                              issuer_session: credentialOffer.issuer_session,
+                                              proof: proof)
+        return rawRequest
+    }
+    
+    private func formatProof(configurationId: String,
+                             credentialIssuer: String,
+                             accessToken: String) throws -> String
+    {
+        guard let identifier = try? configuration.identifierManager.fetchOrCreateMasterIdentifier(),
+              let signingKey = identifier.didDocumentKeys.first else
+        {
+            throw FormatterError.noSigningKeyFound
+        }
+        
+//        guard let accessToken = Data(base64URLEncoded: accessToken) else
+//        {
+//            throw OpenId4VCIValidationError.MalformedCredentialMetadata(message: "")
+//        }
+//        
+//        let hashedAccessToken = Sha256().hash(data: accessToken).base64URLEncodedString()
+        
+        let claims = OpenID4VCIJWTProofClaims(credentialIssuer: credentialIssuer,
+                                              credentialConfigurationId: configurationId,
+                                              did: identifier.longFormDid,
+                                              accessTokenHash: accessToken)
+        
+        let headers = headerFormatter.formatHeaders(usingIdentifier: identifier,
+                                                    andSigningKey: signingKey,
+                                                    type: "jwt") // openid4vci-proof+jwt
+        
+        guard var jwt = JwsToken(headers: headers, content: claims) else
+        {
+            throw OpenId4VCIValidationError.MalformedCredentialMetadata(message: "")
+        }
+        
+        try jwt.sign(using: signer, withSecret: signingKey.keyReference)
+        
+        // sign JWT
+        return try jwt.serialize()
     }
 }
