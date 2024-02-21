5fe4190532a5282eeec73861f3dda6d5adbd8413
Sydney Morton <symorton@microsoft.com>
2024-02-21 11:48:40 -0800

    Add tests for Formatter.

---
+++ WalletLibrary/WalletLibraryTests/Mocks/MockIdentifierManager.swift
@@ -0,0 +1,53 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+@testable import WalletLibrary
+
+struct MockIdentifierManager: IdentifierManager
+{
+    enum ExpectedError: Error
+    {
+        case ExpectedToThrow
+    }
+    
+    let mockKeyId: String
+    
+    let doesThrow: Bool
+    
+    init(mockKeyId: String)
+    {
+        self.mockKeyId = mockKeyId
+        self.doesThrow = false
+    }
+    
+    init(doesThrow: Bool = false)
+    {
+        self.mockKeyId = "mockKeyId"
+        self.doesThrow = doesThrow
+    }
+    
+    func fetchOrCreateMasterIdentifier() throws -> Identifier
+    {
+        guard !doesThrow else
+        {
+            throw ExpectedError.ExpectedToThrow
+        }
+        
+        return mockIdentifier(keyId: mockKeyId)
+    }
+    
+    private func mockIdentifier(keyId: String) -> Identifier
+    {
+        let uuid = UUID()
+        let key = KeyContainer(keyReference: MockCryptoSecret(id: uuid), keyId: keyId)
+        let identifier = Identifier(longFormDid: "did:test:1234",
+                                    didDocumentKeys: [key],
+                                    updateKey: key,
+                                    recoveryKey: key,
+                                    alias: "mock alias")
+        return identifier
+    }
+}
+
