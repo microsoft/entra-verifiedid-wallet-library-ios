0a2afd4f50dce13db462756144775b6fa2e36d99
Sydney Morton <symorton@microsoft.com>
2024-03-05 11:16:46 -0800

    Create OpenID4VCIPreAuthTokenPostOperation.swift

---
+++ WalletLibrary/WalletLibrary/Networking/Operations/OpenID4VCIPreAuthTokenPostOperation.swift
@@ -0,0 +1,44 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+/**
+ * The Network Operation for the Pre Authorization Token Retrieval.
+ */
+struct OpenID4VCIPreAuthTokenPostOperation: WalletLibraryPostOperation
+{
+    typealias RequestBody = PreAuthTokenRequest
+    typealias ResponseBody = PreAuthTokenResponse
+    
+    /// Post body should be formatted as URL Encoded String.
+    let encoder = FormURLEncodedEncoder()
+    
+    let urlSession: URLSession
+    
+    var urlRequest: URLRequest
+    
+    var correlationVector: VerifiedIdCorrelationHeader?
+    
+    init(requestBody: PreAuthTokenRequest,
+         url: URL,
+         additionalHeaders: [String : String]?,
+         urlSession: URLSession,
+         correlationVector: VerifiedIdCorrelationHeader?) throws
+    {
+        
+        self.urlSession = urlSession
+        self.correlationVector = correlationVector
+        self.urlRequest = URLRequest(url: url)
+        self.urlRequest.httpMethod = Constants.POST
+        self.urlRequest.httpBody = try self.encoder.encode(value: requestBody)
+        self.urlRequest.setValue(Constants.FORM_URLENCODED,
+                                 forHTTPHeaderField: Constants.CONTENT_TYPE)
+        
+        /// Adds value to prefer header, appending if value already exists.
+        let preferHeader = [OpenID4VCINetworkConstants.PreferHeaderField: OpenID4VCINetworkConstants.InteropProfileVersion]
+        addHeadersToURLRequest(headers: preferHeader)
+        
+        addHeadersToURLRequest(headers: additionalHeaders)
+    }
+}
