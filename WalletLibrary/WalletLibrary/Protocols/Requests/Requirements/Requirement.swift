ffecf6e0a41d879e97b05b51588d3ff109c2de4b
Sydney Morton <symorton@microsoft.com>
2024-03-11 15:51:33 -0700

    Add helper method to reduce a requirement.

--- WalletLibrary/WalletLibrary/Protocols/Requests/Requirements/Requirement.swift
+++ WalletLibrary/WalletLibrary/Protocols/Requests/Requirements/Requirement.swift
@@ -6,10 +6,35 @@
 /**
  * An object that describes a necessary piece of information to be included within a Request.
  */
-public protocol Requirement: AnyObject {
+public protocol Requirement: AnyObject 
+{
     /// Whether or not the requirement is required to fulfill request.
     var required: Bool { get }
     
     /// Validate the requirement, and throw if there is something invalid.
     func validate() -> VerifiedIdResult<Void>
+}
+
+extension [Requirement]
+{
+    func reduce() throws -> Requirement
+    {
+        if count == 1,
+           let onlyRequirement = first
+        {
+            return onlyRequirement
+        }
+        else if count > 1
+        {
+            let groupRequirement = GroupRequirement(required: true,
+                                                    requirements: self,
+                                                    requirementOperator: .ALL)
+            return groupRequirement
+        }
+        else
+        {
+            let errorMessage = "Grant types on Credential Offer not supported."
+            throw OpenId4VCIValidationError.MalformedCredentialOffer(message: errorMessage)
+        }
+    }
 }
