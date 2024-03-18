17ab4fecc702ca3ad997819b1901bb47844963d1
Sydney <symorton@microsoft.com>
2024-03-18 10:40:36 -0700

    Add Networking components for Issuance V2 Pre Auth Flow. (#84)

---
+++ WalletLibrary/WalletLibrary/Networking/Encoders/FormURLEncodedRequestEncoder.swift
@@ -0,0 +1,24 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
+/**
+ * An encoder to handle Post request bodies in the form of URLEncoded Strings.
+ */
+struct FormURLEncodedRequestEncoder: Encoding
+{
+    /// The Request Body must adhere to the `PropertyIterable` protocol.
+    typealias RequestBody = PropertyIterable
+    
+    /// Encode the Request Body in the form of a URLEncoded String.
+    func encode(value: RequestBody) throws -> Data
+    {
+        let encodedString = value.allProperties().toURLEncodedString()
+        
+        let data = try Data.getRequiredProperty(property: encodedString.data(using: .ascii),
+                                                propertyName: "encoded_string")
+        
+        return data
+    }
+}
