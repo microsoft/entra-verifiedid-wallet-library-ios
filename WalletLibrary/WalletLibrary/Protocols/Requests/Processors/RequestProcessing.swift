20dd46d8e47f2a162c40c5a3d7c529ed88bccbb9
Sydney Morton <symorton@microsoft.com>
2024-03-21 10:18:58 -0700

    Add Public protocol that support Request Processors that can be extended.

--- WalletLibrary/WalletLibrary/Protocols/Requests/Processors/RequestProcessing.swift
+++ WalletLibrary/WalletLibrary/Protocols/Requests/Processors/RequestProcessing.swift
@@ -7,7 +7,19 @@
  * An object that can handle an input where the request can be resolved by a request resolver and
  * then processed, validated and mapped to a verified id request. A conforming object is request protocol specific.
  */
-public protocol RequestProcessing 
+protocol RequestProcessing: ExtendableRequestProcessing
+{
+    /// Determines if Request Handler can handle the object.
+    func canHandle(rawRequest: Any) -> Bool
+    
+    /// Validate and map an input to a verified id request.
+    func handle(rawRequest: Any) async throws -> any VerifiedIdRequest
+}
+
+/**
+ * A   `RequestProcessor` that is able to be extended using `RequestProcessorExtendable`s. 
+ */
+public protocol ExtendableRequestProcessing
 {
     /**
      * Associated type formed by the RequestProcessor to pass to extensions.
@@ -17,10 +29,4 @@ public protocol RequestProcessing
     
     /// Extensions to this RequestProcessor. All extensions should be called after main request processing.
     var requestProcessorExtensions: [any RequestProcessorExtendable] { get set }
-    
-    /// Determines if Request Handler can handle the object.
-    func canHandle(rawRequest: Any) -> Bool
-    
-    /// Validate and map an input to a verified id request.
-    func handle(rawRequest: Any) async throws -> any VerifiedIdRequest
 }
