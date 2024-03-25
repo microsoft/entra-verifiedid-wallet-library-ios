cc26c690d72cf88151c58fddfa988b4ad0fb2812
Sydney Morton <symorton@microsoft.com>
2024-03-25 13:13:17 -0700

    Update PresentationExchangeSerializer.swift

--- WalletLibrary/WalletLibrary/Requests/OpenId/PresentationExchange/PresentationExchangeSerializer.swift
+++ WalletLibrary/WalletLibrary/Requests/OpenId/PresentationExchange/PresentationExchangeSerializer.swift
@@ -8,23 +8,19 @@
  */
 class PresentationExchangeSerializer: RequestProcessorSerializing
 {
-    private enum Constants
-    {
-        static let JwtVc = "jwt_vc"
-        static let JwtVp = "jwt_vp"
-    }
-    
     private let configuration: LibraryConfiguration
     
     private let vpFormatter: VerifiablePresentationFormatter
     
-    private var partialSubmissionMap: [PartialSubmissionMapEntry]
+    private var partialSubmissionMap: Set<PartialSubmissionMapEntry>
     
     private let state: String
     
     private let audience: String = ""
     
     private let nonce: String = ""
+    
+    private let definitionId: String = ""
     
     init(state: String,
          libraryConfiguration: LibraryConfiguration,
@@ -47,8 +43,9 @@ class PresentationExchangeSerializer: RequestProcessorSerializing
                                                  verifiedIdSerializer: verifiedIdSerializer) as? String
         {
             let entry = PartialSubmissionMapEntry(rawVC: rawVC,
-                                                  peRequirement: peRequirement)
-            partialSubmissionMap.append(entry)
+                                                  peRequirement: peRequirement,
+                                                  inputDescriptorId: peRequirement.inputDescriptorId)
+            partialSubmissionMap.insert(entry)
         }
     }
     
@@ -72,6 +69,34 @@ class PresentationExchangeSerializer: RequestProcessorSerializing
     
     private func buildIdToken() throws -> PresentationResponseToken
     {
+        //1. sort VCs into VPs
+        var index = 0
+        let vpGroups = partialSubmissionMap.reduce([VPGroup(index: index)]) { partialGroups, entry in
+            
+            for group in partialGroups
+            {
+                if group.canInclude(entry: entry)
+                {
+                    group.add(entry: entry)
+                    return partialGroups
+                }
+            }
+            
+            index = index + 1
+            let newGroup = VPGroup(index: index)
+            newGroup.add(entry: entry)
+            var newGroups = partialGroups
+            newGroups.append(newGroup)
+            return newGroups
+        }
+        
+        let inputDescriptors = vpGroups.flatMap { $0.buildInputDescriptors() }
+        
+        
+        let submission = PresentationSubmission(id: UUID().uuidString,
+                                                definitionId: definitionId,
+                                                inputDescriptorMap: inputDescriptors)
+        
         throw VerifiedIdError(message: "", code: "")
     }
     
@@ -120,9 +145,75 @@ class PresentationExchangeSerializer: RequestProcessorSerializing
 //    }
 }
 
-struct PartialSubmissionMapEntry
+struct PartialSubmissionMapEntry: Hashable
 {
     let rawVC: String
     
     let peRequirement: PresentationExchangeRequirement
+    
+    let inputDescriptorId: String
+    
+    func hash(into hasher: inout Hasher) 
+    {
+        hasher.combine(inputDescriptorId)
+    }
+    
+    static func == (lhs: PartialSubmissionMapEntry, rhs: PartialSubmissionMapEntry) -> Bool
+    {
+        return lhs.inputDescriptorId == rhs.inputDescriptorId
+    }
+}
+
+class VPGroup
+{
+    private enum Constants
+    {
+        static let JwtVc = "jwt_vc"
+        static let JwtVp = "jwt_vp"
+    }
+    
+    let id: UUID
+    
+    let index: Int
+    
+    var partials: [PartialSubmissionMapEntry]
+    
+    init(index: Int)
+    {
+        self.index = index
+        id = UUID()
+        partials = []
+    }
+    
+    func canInclude(entry: PartialSubmissionMapEntry) -> Bool
+    {
+        // same subject and exclusive with
+        return true
+    }
+    
+    func add(entry: PartialSubmissionMapEntry)
+    {
+        partials.append(entry)
+    }
+    
+    func buildInputDescriptors() -> [InputDescriptorMapping]
+    {
+        let mappings = partials.enumerated().map { (vcIndex, entry) in
+            self.build(vcIndex: vcIndex, entry: entry)
+        }
+        return mappings
+    }
+    
+    private func build(vcIndex: Int, entry: PartialSubmissionMapEntry) -> InputDescriptorMapping
+    {
+        let vcPath = "$[\(index)].verifiableCredential[\(vcIndex)]"
+        let nestedInputDescriptorMapping = NestedInputDescriptorMapping(id: entry.inputDescriptorId,
+                                                                        format: Constants.JwtVc,
+                                                                        path: vcPath)
+        
+        return InputDescriptorMapping(id: entry.inputDescriptorId,
+                                      format: Constants.JwtVp,
+                                      path: "$[\(index)]",
+                                      pathNested: nestedInputDescriptorMapping)
+    }
 }
