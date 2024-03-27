42348b48fbb2189a04e985d7ce62ae353df25f0d
Sydney Morton <symorton@microsoft.com>
2024-03-27 13:03:11 -0700

    Add comments to PartialInputDescriptor.

--- WalletLibrary/WalletLibrary/Requests/OpenId/PresentationExchange/PartialInputDescriptor.swift
+++ WalletLibrary/WalletLibrary/Requests/OpenId/PresentationExchange/PartialInputDescriptor.swift
@@ -16,10 +16,13 @@ struct PartialInputDescriptor
         static let JwtVp = "jwt_vp"
     }
     
+    /// The serialized value of the Verified Id associated with this input descriptor.
     let serializedVerifiedId: String
     
+    /// The ID from the request.
     private let inputDescriptorId: String
     
+    /// The requirement fulfilled by this Input Descriptor.
     private let requirement: PresentationExchangeRequirement
     
     init(serializedVerifiedId: String, requirement: PresentationExchangeRequirement)
@@ -29,6 +32,11 @@ struct PartialInputDescriptor
         self.requirement = requirement
     }
     
+    /// Determines if this partial input descriptor is compatible with another partial input descriptor.
+    /// Compatibility is defined based on the exclusivity criteria specified in their presentation exchange
+    /// requirements. If both input descriptors have exclusive presentation criteria that do not conflict
+    /// with each other's identifier, they are considered compatible.
+    /// This method will grow as we support more compatibility constraints such as Verified ID subject.
     func isCompatibleWith(partialInputDescriptor: PartialInputDescriptor) -> Bool
     {
         let nonCompatIdsFromEntry = partialInputDescriptor.requirement.exclusivePresentationWith ?? []
@@ -38,6 +46,7 @@ struct PartialInputDescriptor
         return isEntryCompatWithId && isSelfCompWithEntrysId
     }
     
+    /// Builds the complete Input Descriptor for given vcIndex and vpIndex.
     func buildInputDescriptor(vcIndex: Int,
                               vpIndex: Int) -> InputDescriptorMapping
     {
