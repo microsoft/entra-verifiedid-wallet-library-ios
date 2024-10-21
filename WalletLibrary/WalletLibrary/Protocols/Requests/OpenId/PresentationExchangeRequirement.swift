db4edd5b7351d4bff0fadccc783aadfc50e529e9
Sydney <symorton@microsoft.com>
2024-10-21 16:27:49 -0700

    Fix PERequirement to internal/release.

--- WalletLibrary/WalletLibrary/Protocols/Requests/OpenId/PresentationExchangeRequirement.swift
+++ WalletLibrary/WalletLibrary/Protocols/Requests/OpenId/PresentationExchangeRequirement.swift
@@ -4,23 +4,15 @@
 *--------------------------------------------------------------------------------------------*/
 
 /**
- * Presentation Exchange Verified ID formats.
- */
-enum PresentationExchangeVerifiedIdFormat
-{
-    case JWT_VC
-}
-
-/**
  * Describes properties to be included in a Presentation Exchange Requirement.
  */
-protocol PresentationExchangeRequirement
+public protocol PresentationExchangeRequirement
 {
     /// The id of the presentationDescriptior's input_descriptor object. This is required for forming the input descriptor's submission map.
     var inputDescriptorId: String { get }
     
     /// Specifies the output format of the credential that fulfills this requirement.
-    var format: PresentationExchangeVerifiedIdFormat { get }
+    var format: String { get }
     
     /// Optional list of other input descriptor IDs that this submission should not be presented with in the same cryptographic proof.
     var exclusivePresentationWith: [String]? { get set }
