6c946ccd9c4d72a756a88e5868d50505c5fdc8ae
Sydney <symorton@microsoft.com>
2024-10-29 13:17:06 -0700

    Update VerifiedIdClientBuilder.swift

--- WalletLibrary/WalletLibrary/VerifiedIdClientBuilder.swift
+++ WalletLibrary/WalletLibrary/VerifiedIdClientBuilder.swift
@@ -35,7 +35,7 @@ public class VerifiedIdClientBuilder
     /// Builds the VerifiedIdClient with the set configuration from the builder.
     public func build() -> VerifiedIdClient 
     {
-        
+
         let previewFeatureFlags = PreviewFeatureFlags(previewFeatureFlags: previewFeatureFlagsSupported)
         let vcLogConsumer = WalletLibraryVCSDKLogConsumer(logger: logger)
         let _ = VerifiableCredentialSDK.initialize(logConsumer: vcLogConsumer,
