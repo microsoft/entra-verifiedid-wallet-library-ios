c8251f70d9c8e75b6cf0fd2aec68ebab43e2664c
Sydney <symorton@microsoft.com>
2024-10-30 14:04:25 -0700

    Clean up new Validator class.

--- WalletLibrary/Submodules/VerifiableCredential-SDK-iOS/VCServices/VCServices/PresentationService.swift
+++ WalletLibrary/Submodules/VerifiableCredential-SDK-iOS/VCServices/VCServices/PresentationService.swift
@@ -3,77 +3,55 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-enum PresentationServiceError: Error {
-    case inputStringNotUri
-    case noKeysSavedForIdentifier
-    case noQueryParametersOnUri
-    case noValueForRequestUriQueryParameter
-    case noRequestUriQueryParameter
-    case unableToCastToPresentationResponseContainer
-    case unableToFetchIdentifier
-    case noKeyIdInRequestHeader
-    case noPublicKeysInIdentifierDocument
-    case noIssuerIdentifierInRequest
-}
-
-// TODO: replace during FIPS work.
+/// A validator class responsible for validating OpenID presentation requests.
+/// It interacts with various services to ensure the request is properly verified
+/// and trusted before proceeding with further actions.
 class OpenIdPresentationRequestValidator
 {
-    let formatter: PresentationResponseFormatting
-    let presentationApiCalls: PresentationNetworking
+    /// API for making discovery calls to fetch DID documents. TODO use new networking layer for this.
     private let didDocumentDiscoveryApiCalls: DiscoveryNetworking
+    
+    /// Validator for checking the authenticity and structure of presentation requests.
     private let requestValidator: RequestValidating
+    
+    /// Service for validating the linked domain associated with a DID document.
     private let linkedDomainService: LinkedDomainService
-    private let identifierService: IdentifierService
-    private let sdkLog: VCSDKLog
     
     convenience init(correlationVector: VerifiedIdCorrelationHeader? = nil,
                      rootOfTrustResolver: RootOfTrustResolver?,
-                     urlSession: URLSession) {
-        self.init(formatter: PresentationResponseFormatter(sdkLog: VCSDKLog.sharedInstance),
-                  presentationApiCalls: PresentationNetworkCalls(correlationVector: correlationVector,
-                                                                 urlSession: urlSession),
-                  didDocumentDiscoveryApiCalls: DIDDocumentNetworkCalls(correlationVector: correlationVector,
+                     urlSession: URLSession) 
+    {
+        self.init(didDocumentDiscoveryApiCalls: DIDDocumentNetworkCalls(correlationVector: correlationVector,
                                                                         urlSession: urlSession),
                   requestValidator: PresentationRequestValidator(),
                   linkedDomainService: LinkedDomainService(correlationVector: correlationVector,
                                                            rootOfTrustResolver: rootOfTrustResolver,
-                                                           urlSession: urlSession),
-                  identifierService: IdentifierService(),
-                  sdkLog: VCSDKLog.sharedInstance)
+                                                           urlSession: urlSession))
     }
     
-    init(formatter: PresentationResponseFormatting,
-         presentationApiCalls: PresentationNetworking,
-         didDocumentDiscoveryApiCalls: DiscoveryNetworking,
+    init(didDocumentDiscoveryApiCalls: DiscoveryNetworking,
          requestValidator: RequestValidating,
-         linkedDomainService: LinkedDomainService,
-         identifierService: IdentifierService,
-         sdkLog: VCSDKLog = VCSDKLog.sharedInstance) {
-        self.formatter = formatter
-        self.presentationApiCalls = presentationApiCalls
+         linkedDomainService: LinkedDomainService)
+    {
         self.didDocumentDiscoveryApiCalls = didDocumentDiscoveryApiCalls
         self.requestValidator = requestValidator
         self.linkedDomainService = linkedDomainService
-        self.identifierService = identifierService
-        self.sdkLog = sdkLog
     }
     
-//    func getRequest(usingUrl urlStr: String) async throws -> PresentationRequest {
-//        return try await logTime(name: "Presentation getRequest") {
-//            let requestUri = try self.getRequestUri(from: urlStr)
-//            let request = try await self.presentationApiCalls.getRequest(withUrl: requestUri)
-//            return try await self.validate(request: request)
-//        }
-//    }
-    
-    func validate(request: PresentationRequestToken) async throws -> PresentationRequest {
+    /// Validates the given presentation request token by checking its identifier document,
+    /// verifying the associated public keys, and confirming the linked domain.
+    ///
+    /// - Parameter request: The token representing the presentation request.
+    /// - Returns: A validated `PresentationRequest` if successful.
+    /// - Throws: An error if validation fails at any step.
+    func validate(request: PresentationRequestToken) async throws -> PresentationRequest
+    {
         
         let document = try await self.getIdentifierDocument(from: request)
         
         guard let publicKeys = document.verificationMethod else
         {
-            throw PresentationServiceError.noPublicKeysInIdentifierDocument
+            throw VerifiedIdErrors.MalformedInput(message: "No Public Keys in Identifier Document").error
         }
         
         try self.requestValidator.validate(request: request, usingKeys: publicKeys)
@@ -81,50 +59,21 @@ class OpenIdPresentationRequestValidator
         return PresentationRequest(from: request, linkedDomainResult: result)
     }
     
-//    func send(response: PresentationResponseContainer) async throws {
-//        try await logTime(name: "Presentation sendResponse") {
-//            let formattedResponse = try self.formatPresentationResponse(response: response)
-//            try await self.presentationApiCalls.sendResponse(usingUrl: response.audienceUrl,
-//                                                             withBody: formattedResponse)
-//        }
-//    }
-    
-//    private func getRequestUri(from urlStr: String) throws -> String {
-//        
-//        guard let urlComponents = URLComponents(string: urlStr) else { throw PresentationServiceError.inputStringNotUri }
-//        guard let queryItems = urlComponents.percentEncodedQueryItems else { throw PresentationServiceError.noQueryParametersOnUri }
-//        
-//        for queryItem in queryItems {
-//            if queryItem.name == ServicesConstants.REQUEST_URI {
-//                guard let value = queryItem.value?.removingPercentEncoding
-//                else { throw PresentationServiceError.noValueForRequestUriQueryParameter }
-//                return value
-//            }
-//        }
-//        
-//        throw PresentationServiceError.noRequestUriQueryParameter
-//    }
-    
-    private func getIdentifierDocument(from token: PresentationRequestToken) async throws -> IdentifierDocument {
+    private func getIdentifierDocument(from token: PresentationRequestToken) async throws -> IdentifierDocument 
+    {
         let did = try getDIDFromHeader(request: token)
         return try await didDocumentDiscoveryApiCalls.getDocument(from: did)
     }
     
-    private func getDIDFromHeader(request: PresentationRequestToken) throws -> String {
+    private func getDIDFromHeader(request: PresentationRequestToken) throws -> String 
+    {
         
         guard let kid = request.headers.keyId?.split(separator: ServicesConstants.FRAGMENT_SEPARATOR),
-              let did = kid.first else {
-            
-            throw PresentationServiceError.noKeyIdInRequestHeader
+              let did = kid.first else 
+        {
+            throw VerifiedIdErrors.MalformedInput(message: "No DID in request header.").error
         }
         
         return String(did)
-    }
-    
-    private func formatPresentationResponse(response: PresentationResponseContainer) throws -> PresentationResponse {
-        let identifier = try identifierService.fetchOrCreateMasterIdentifier()
-        sdkLog.logVerbose(message: "Signing Presentation Response with Identifier")
-        return try self.formatter.format(response: response,
-                                         usingIdentifier: identifier)
     }
 }
