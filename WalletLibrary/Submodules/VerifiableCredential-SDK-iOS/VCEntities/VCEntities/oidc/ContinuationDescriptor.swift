fa4d1ba959c6843e4e237b9923507eef3da0e8b8
Logan Girvin <logirvin@microsoft.com>
2025-09-18 15:58:00 -0700

    Adding correlation ID

--- WalletLibrary/Submodules/VerifiableCredential-SDK-iOS/VCEntities/VCEntities/oidc/ContinuationDescriptor.swift
+++ WalletLibrary/Submodules/VerifiableCredential-SDK-iOS/VCEntities/VCEntities/oidc/ContinuationDescriptor.swift
@@ -15,15 +15,20 @@ public struct ContinuationDescriptor: Codable, Equatable {
     /// continuation payload
     public let payload: String?
     
+    /// telemetry correlation id
+    public let correlationId: String?
+    
     public init(upn: String?,
          url: String?,
-         payload: String?) {
+         payload: String?,
+         correlationId: String?) {
         self.upn = upn
         self.url = url
         self.payload = payload
+        self.correlationId = correlationId
     }
 
     enum CodingKeys: String, CodingKey {
-        case upn, url, payload
+        case upn, url, payload, correlationId
     }
 }
